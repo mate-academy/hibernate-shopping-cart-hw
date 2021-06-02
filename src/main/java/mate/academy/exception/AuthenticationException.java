edfa5d28597774d74acb8c58b3ac40e41d7a7933
mankokolya <mankokolya@gmail.com>
2021-06-02 16:16:14 +0300

    shopping cart task done

--- src/main/java/mate/academy/exception/AuthenticationException.java
+++ src/main/java/mate/academy/exception/AuthenticationException.java
@@ -1,8 +1,14 @@
 package mate.academy.exception;
 
+import org.hibernate.exception.ConstraintViolationException;
+
 public class AuthenticationException extends Exception {
 
     public AuthenticationException(String message) {
         super(message);
+    }
+
+    public AuthenticationException(String message, ConstraintViolationException exception) {
+        super(message, exception);
     }
 }
---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,43 @@
+package mate.academy.service.impl;
+
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private TicketDao ticketDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        ShoppingCart shoppingCart = shoppingCartDao.getByUser(user);
+        Ticket savedTicket = ticketDao.add(new Ticket(user, movieSession));
+        shoppingCart.getTickets().add(savedTicket);
+        shoppingCartDao.update(shoppingCart);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return shoppingCartDao.getByUser(user);
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        shoppingCartDao.add(new ShoppingCart(user));
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.setTickets(null);
+        shoppingCartDao.update(shoppingCart);
+    }
+}
