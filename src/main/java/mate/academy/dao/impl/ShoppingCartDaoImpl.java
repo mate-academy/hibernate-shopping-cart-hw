a2d068d62230a0051338ecbc9b1a89780589b75d
safordog <safordog@gmail.com>
2021-06-07 23:58:28 +0300

    fixed getByUser()

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -39,12 +39,11 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return session.createQuery("FROM ShoppingCart sc "
                     + "LEFT JOIN FETCH sc.tickets t "
-                    + "LEFT JOIN FETCH sc.user "
                     + "LEFT JOIN FETCH t.movieSession ms "
                     + "LEFT JOIN FETCH ms.movie "
                     + "LEFT JOIN FETCH ms.cinemaHall "
-                    + "WHERE sc.id = :id", ShoppingCart.class)
-                    .setParameter("id", user.getId()).getSingleResult();
+                    + "WHERE sc.user = :user", ShoppingCart.class)
+                    .setParameter("user", user).getSingleResult();
         } catch (Exception e) {
             throw new DataProcessingException("Can't get shopping cart by user: " + user, e);
         }
--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -2,7 +2,6 @@ package mate.academy.model;
 
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
@@ -18,7 +17,7 @@ public class ShoppingCart {
     @OneToMany
     @JoinColumn(name = "shopping_cart_id")
     private List<Ticket> tickets;
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne
     @MapsId
     private User user;
 
