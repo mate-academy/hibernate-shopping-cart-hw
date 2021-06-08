10415c2b97ecebc9ecf5e38d08d5bfa3e10dc3c4
fotomassimo <maksym.pavliuchenko@gmail.com>
2021-06-08 22:55:55 +0300

    initial commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,17 +2,38 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.ShoppingCartService;
+import mate.academy.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    public static final Logger logger = LogManager.getLogger(Main.class);
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService = (MovieService)
+            injector.getInstance(MovieService.class);
+    private static final CinemaHallService cinemaHallService = (CinemaHallService)
+            injector.getInstance(CinemaHallService.class);
+    private static final MovieSessionService movieSessionService = (MovieSessionService)
+            injector.getInstance(MovieSessionService.class);
+    private static final UserService userService = (UserService)
+            injector.getInstance(UserService.class);
+    private static final AuthenticationService authenticationService = (AuthenticationService)
+            injector.getInstance(AuthenticationService.class);
+    private static final ShoppingCartService shoppingCartService = (ShoppingCartService)
+            injector.getInstance(ShoppingCartService.class);
 
+    public static void main(String[] args) {
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
@@ -27,7 +48,6 @@ public class Main {
         secondCinemaHall.setCapacity(200);
         secondCinemaHall.setDescription("second hall with capacity 200");
 
-        CinemaHallService cinemaHallService = null;
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
 
@@ -44,12 +64,42 @@ public class Main {
         yesterdayMovieSession.setMovie(fastAndFurious);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-        MovieSessionService movieSessionService = null;
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                         fastAndFurious.getId(), LocalDate.now()));
+
+        System.out.println("--------user service--------");
+        System.out.println(authenticationService.register("alice@gmail.com", "1234"));
+        System.out.println(authenticationService.register("bob@gmail.com", "bob1234"));
+        User bob = userService.findByEmail("bob@gmail.com").get();
+        User alice = userService.findByEmail("alice@gmail.com").get();
+        System.out.println(bob);
+        System.out.println(alice);
+        try {
+            System.out.println(authenticationService.login("a_lice@gmail.com", "1234"));
+        } catch (AuthenticationException e) {
+            logger.error("Authentication was unsuccessful: incorrect email", e);
+        }
+        try {
+            System.out.println(authenticationService.login("alice@gmail.com", "11111"));
+        } catch (AuthenticationException e) {
+            logger.error("Authentication was unsuccessful: incorrect password", e);
+        }
+        try {
+            System.out.println(authenticationService.register("alice@gmail.com", "11111"));
+        } catch (RuntimeException e) {
+            logger.error(e.getMessage());
+        }
+
+        System.out.println("------shopping cart service------");
+        System.out.println(shoppingCartService.getByUser(bob));
+        shoppingCartService.addSession(tomorrowMovieSession, bob);
+        shoppingCartService.addSession(yesterdayMovieSession, bob);
+        System.out.println(shoppingCartService.getByUser(bob));
+        shoppingCartService.clear(shoppingCartService.getByUser(bob));
+        System.out.println(shoppingCartService.getByUser(bob));
     }
 }
--- src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
@@ -1,50 +1,38 @@
 package mate.academy.dao.impl;
 
-import java.util.Optional;
-import mate.academy.dao.UserDao;
+import mate.academy.dao.TicketDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
-import mate.academy.model.User;
+import mate.academy.model.Ticket;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
 
 @Dao
-public class UserDaoImpl implements UserDao {
+public class TicketDaoImpl implements TicketDao {
     private final SessionFactory factory = HibernateUtil.getSessionFactory();
 
     @Override
-    public User add(User user) {
+    public Ticket add(Ticket ticket) {
         Session session = null;
         Transaction transaction = null;
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.save(user);
+            session.save(ticket);
             transaction.commit();
-            return user;
+            return ticket;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't insert to DB user: "
-                    + user, e);
+            throw new DataProcessingException("Can't insert to DB ticket: "
+                    + ticket, e);
         } finally {
             if (session != null) {
                 session.close();
             }
-        }
-    }
-
-    @Override
-    public Optional<User> findByEmail(String email) {
-        try (Session session = factory.openSession()) {
-            Query<User> query = session.createQuery("FROM User u "
-                    + "WHERE u.email = :email", User.class);
-            query.setParameter("email", email);
-            return query.uniqueResultOptional();
         }
     }
 }
