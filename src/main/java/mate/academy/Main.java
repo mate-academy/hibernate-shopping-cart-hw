84165629b53dd17289e41c448aa8d962cf064779
dvdovynets <vdovinec@gmail.com>
2021-06-09 19:24:55 +0300

    added one more FetchTYPE.LAZY

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -91,8 +91,6 @@ public class Main {
         }
         shoppingCartService.addSession(tomorrowMovieSession, bob);
         ShoppingCart bobsCart = shoppingCartService.getByUser(bob);
-        System.out.println(bobsCart);
         shoppingCartService.clear(bobsCart);
-        System.out.println(bobsCart);
     }
 }
