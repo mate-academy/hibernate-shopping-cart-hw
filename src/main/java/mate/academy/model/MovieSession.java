640da94103496b91a3982b20505e648bca228438
unknown <vovazalli@gmail.com>
2021-06-09 19:56:01 +0300

    First solution

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -2,7 +2,6 @@ package mate.academy.model;
 
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,47 +14,47 @@ public class MovieSession {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     private Movie movie;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     private CinemaHall cinemaHall;
     private LocalDateTime showTime;
-
+    
     public MovieSession() {
     }
-
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public Movie getMovie() {
         return movie;
     }
-
+    
     public void setMovie(Movie movie) {
         this.movie = movie;
     }
-
+    
     public CinemaHall getCinemaHall() {
         return cinemaHall;
     }
-
+    
     public void setCinemaHall(CinemaHall cinemaHall) {
         this.cinemaHall = cinemaHall;
     }
-
+    
     public LocalDateTime getShowTime() {
         return showTime;
     }
-
+    
     public void setShowTime(LocalDateTime showTime) {
         this.showTime = showTime;
     }
-
+    
     @Override
     public String toString() {
         return "MovieSession{"
