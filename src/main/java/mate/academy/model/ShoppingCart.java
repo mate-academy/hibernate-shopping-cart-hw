3ef0fb8bd09f362146eafbda35acd2e6631e87ae
Maksim <max.freedman.main@gmail.com>
2021-06-09 17:14:05 +0300

    completed models impl

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,5 +1,48 @@
 package mate.academy.model;
 
-public class ShoppingCart {
+import java.util.List;
 
+public class ShoppingCart {
+    private Long id;
+    private List<Ticket> tickets;
+    private User user;
+
+    public ShoppingCart() {
+    }
+
+    public ShoppingCart(List<Ticket> tickets, User user) {
+        this.tickets = tickets;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" + "id=" + id
+                + ", tickets=" + tickets
+                + ", user=" + user + '}';
+    }
 }
--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -1,5 +1,46 @@
 package mate.academy.model;
 
 public class Ticket {
+    private Long id;
+    private MovieSession movieSession;
+    private User user;
 
+    public Ticket() {
+    }
+
+    public Ticket(MovieSession movieSession, User user) {
+        this.movieSession = movieSession;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" + "id=" + id
+                + ", movieSession=" + movieSession
+                + ", user=" + user + '}';
+    }
 }
