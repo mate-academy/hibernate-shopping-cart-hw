8f1f0f98c159d1ee18ab8248f3c5522e661fc9d7
Maksim <max.freedman.main@gmail.com>
2021-06-09 17:37:40 +0300

    completed models impl

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,10 +1,26 @@
 package mate.academy.model;
 
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "shopping_carts")
 public class ShoppingCart {
+    @Id
     private Long id;
+
+    @OneToMany
+    @JoinColumn(name = "ticket_id")
     private List<Ticket> tickets;
+
+    @OneToOne
+    @MapsId
     private User user;
 
     public ShoppingCart() {
--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -1,8 +1,24 @@
 package mate.academy.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private MovieSession movieSession;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     public Ticket() {
