164a89c151772a49ac078eecdc64e24c9123fb77
Iryna Ustymenko <i.ustimenko18@gmail.com>
2021-06-09 19:27:45 +0300

    created all

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -6,6 +6,7 @@ import java.security.SecureRandom;
 
 public class HashUtil {
     public static final String HASH_ALGORITHM = "SHA-512";
+    private static final int size = 16;
 
     public static String hashPassword(String password, byte[] salt) {
         StringBuilder hashedPwd = new StringBuilder();
@@ -18,13 +19,14 @@ public class HashUtil {
             }
             return hashedPwd.toString();
         } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Can`t hash password!", e);
+            throw new RuntimeException("Could create hash using " + HASH_ALGORITHM
+                    + " algorithm", e);
         }
     }
 
     public static byte[] getSalt() {
         SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
+        byte[] salt = new byte[size];
         random.nextBytes(salt);
         return salt;
     }
