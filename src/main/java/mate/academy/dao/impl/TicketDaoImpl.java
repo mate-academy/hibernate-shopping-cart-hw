3d9b1767d7d741d433f6ef5856f45b70e5079290
Maksim <max.freedman.main@gmail.com>
2021-06-09 18:13:20 +0300

    completed TicektDaoImpl

--- src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
@@ -1,11 +1,42 @@
 package mate.academy.dao.impl;
 
 import mate.academy.dao.TicketDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
 import mate.academy.model.Ticket;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+@Dao
 public class TicketDaoImpl implements TicketDao {
+    private final SessionFactory sessionFactory;
+
+    public TicketDaoImpl() {
+        sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
     @Override
     public Ticket add(Ticket ticket) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(ticket);
+            transaction.commit();
+            return ticket;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert ticket "
+                    + ticket + " to DB.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
