ec8c1e4db407c5a877910c8e2a486fcd26217646
Koziarevskyi <koziarevskyi@gmail.com>
2021-06-10 15:16:39 +0300

    C003 - Fixed!

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -41,8 +41,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Override
     public void clear(ShoppingCart shoppingCart) {
-        List<Ticket> tickets = Collections.emptyList();
-        shoppingCart.setTickets(tickets);
+        shoppingCart.setTickets(Collections.emptyList());
         shoppingCartDao.update(shoppingCart);
     }
 }
