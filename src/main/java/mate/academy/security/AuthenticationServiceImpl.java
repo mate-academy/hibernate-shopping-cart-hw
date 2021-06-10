0fab19d2396374910f09abd63a6c1da5070db982
the5tranger <mishaantonyuk00@gmail.com>
2021-06-10 12:31:00 +0300

    V1.0.0

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -2,16 +2,21 @@ package mate.academy.security;
 
 import java.util.Optional;
 import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
 import mate.academy.service.UserService;
 import mate.academy.util.HashUtil;
+import org.hibernate.exception.ConstraintViolationException;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
     private UserService userService;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
@@ -19,16 +24,21 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         if (userFromDb.isPresent() && matchPasswords(password, userFromDb.get())) {
             return userFromDb.get();
         }
-        throw new AuthenticationException("Incorrect email or password!");
+        throw new AuthenticationException("Invalid email or password!");
     }
 
     @Override
-    public User register(String email, String password) {
-        User user = new User();
-        user.setEmail(email);
-        user.setPassword(password);
-        userService.add(user);
-        return user;
+    public User register(String email, String password) throws RegistrationException {
+        try {
+            User user = new User();
+            user.setEmail(email);
+            user.setPassword(password);
+            userService.add(user);
+            shoppingCartService.registerNewShoppingCart(user);
+            return user;
+        } catch (ConstraintViolationException e) {
+            throw new RegistrationException("Email is exists! Change the email");
+        }
     }
 
     private boolean matchPasswords(String rawPassword, User userFromDb) {
