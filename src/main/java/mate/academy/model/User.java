438c8ec130376a6b3a19cd3c6d01acf024646b38
PashaTerletskyi <p.terletsky@gmail.com>
2021-08-10 12:40:46 +0300

    Changed toString() User

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -53,6 +53,6 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" + "id=" + id + '}';
+        return "User{" + "id=" + id + ", email='" + email + '\'' + '}';
     }
 }
