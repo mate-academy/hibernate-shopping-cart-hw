4d75f77d3760f779907a00ff0c79499c5f35612c
Anastasia <81781479+aaananas@users.noreply.github.com>
2021-08-10 14:15:59 +0300

    Added constructor

--- src/main/java/mate/academy/exception/RegistrationException.java
+++ src/main/java/mate/academy/exception/RegistrationException.java
@@ -4,4 +4,8 @@ public class RegistrationException extends Exception {
     public RegistrationException(String message, Exception cause) {
         super(message, cause);
     }
+
+    public RegistrationException(String message) {
+        super(message);
+    }
 }
