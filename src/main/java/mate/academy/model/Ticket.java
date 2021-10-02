22337804352103b2a8dce84792280e4774157cba
mishamolnar <michaellmuller423@gmail.com>
2021-10-02 14:41:25 +0300

    services created

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,47 @@
+package mate.academy.model;
+
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MovieSession movieSession;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" + "id=" + id
+                + ", movieSession=" + movieSession
+                + ", user=" + user + '}';
+    }
+}
