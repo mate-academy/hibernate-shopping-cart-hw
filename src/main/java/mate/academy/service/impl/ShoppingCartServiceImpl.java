b8ae3f9998d22bfbc932753b3eebaedbb7799ea8
Kyrylo Dvornyk <kiril75341@gmail.com>
2021-10-28 13:37:05 +0300

    Fixed logic

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -1,7 +1,6 @@
 package mate.academy.service.impl;
 
 import java.util.ArrayList;
-import java.util.Optional;
 import mate.academy.dao.ShoppingCartDao;
 import mate.academy.dao.TicketDao;
 import mate.academy.lib.Inject;
