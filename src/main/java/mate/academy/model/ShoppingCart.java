6caab787ce6245c5e3d5e5bdb0bcd0d7f8a99fb2
Alexandr <netiazhevskyi@gmail.com>
2021-11-01 15:55:57 +0200

    Shopping cart dao + service implemented

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,57 @@
+package mate.academy.model;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToMany
+    private List<Ticket> ticketList;
+    @OneToOne
+    @MapsId
+    private User user;
+
+    public ShoppingCart() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTicketList() {
+        return ticketList;
+    }
+
+    public void setTicketList(List<Ticket> ticketList) {
+        this.ticketList = ticketList;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{id=" + id + ", ticketList=" + ticketList
+                + ", user=" + user + '}';
+    }
+}
