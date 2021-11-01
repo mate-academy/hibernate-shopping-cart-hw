b6e54055070523d8821450b798dcdb26adb5cf52
Pasha Rakitin <dogovor@lan.com.ua>
2021-11-01 16:44:04 +0200

    removed extra join fetch

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -45,7 +45,6 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
                         session.createQuery("FROM ShoppingCart s "
                                     + "LEFT JOIN FETCH s.user "
                                     + "LEFT JOIN FETCH s.tickets t "
-                                    + "LEFT JOIN FETCH t.user "
                                     + "LEFT JOIN FETCH t.movieSession ms "
                                     + "LEFT JOIN FETCH ms.movie "
                                     + "LEFT JOIN FETCH ms.cinemaHall "
