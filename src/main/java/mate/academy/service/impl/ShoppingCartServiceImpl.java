3c116f17bbf0cb20e1e223aab69013e783482446
Sinelife <ya.serzhan@gmail.com>
2021-11-02 17:54:17 +0200

    fixed clear method in ShoppingCartService

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -46,5 +46,6 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public void clear(ShoppingCart shoppingCart) {
         shoppingCart.setTickets(new ArrayList<>());
+        shoppingCartDao.update(shoppingCart);
     }
 }
