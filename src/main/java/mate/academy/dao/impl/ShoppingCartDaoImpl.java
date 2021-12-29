63e82c83f53c69d73477391e3cecbcdb863ca287
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-12-29 22:49:45 +0200

    implemented ticket and ShoppingCart DAO and service layers

---
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,79 @@
+package mate.academy.dao.impl;
+
+import static mate.academy.util.HibernateUtil.getSessionFactory;
+
+import java.util.Optional;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add shopping cart" + shoppingCart
+                    + " to DB.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<ShoppingCart> getByUser(User user) {
+        try (Session session = getSessionFactory().openSession()) {
+            Query<ShoppingCart> getShoppingCartByUser =
+                    session.createQuery("from ShoppingCart sc "
+                            + "left join fetch sc.tickets t "
+                            + "left join fetch sc.user "
+                            + "left join fetch t.movieSession mv "
+                            + "left join fetch mv.movie "
+                            + "left join fetch mv.cinemaHall "
+                            + "where sc.user = :user", ShoppingCart.class);
+            getShoppingCartByUser.setParameter("user", user);
+            return getShoppingCartByUser.uniqueResultOptional();
+        } catch (HibernateException e) {
+            throw new DataProcessingException("Can't find shopping cart by user " + user, e);
+        }
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.update(shoppingCart);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't update shopping cart to " + shoppingCart, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
