263b7a97b52af73bd29c13220a85399c52abc302
Vladyslav Chyver <88184147+Lione2009@users.noreply.github.com>
2021-12-30 11:24:53 +0200

    Update src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
    Co-authored-by: Oleksii-kho <77895678+Oleksii-kho@users.noreply.github.com>

--- src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
@@ -24,7 +24,7 @@ public class TicketDaoImpl implements TicketDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't insert ticket to Db" + ticket, exception);
+            throw new DataProcessingException("Can't insert ticket to DB " + ticket, exception);
         } finally {
             if (session != null) {
                 session.close();
