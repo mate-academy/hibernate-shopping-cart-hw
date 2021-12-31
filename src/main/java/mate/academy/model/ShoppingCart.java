87e1a3458e2197d460b4ae5517028af95b00ac3c
Artik <Artik25942@gmail.com>
2021-12-31 14:41:11 +0200

    all services add

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,52 @@
+package mate.academy.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    private Long id;
+    @OneToOne
+    @MapsId
+    private User user;
+    @OneToMany
+    @JoinTable(name = "shopping_cart_tickets",
+            joinColumns = @JoinColumn(name = "shopping_cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "ticket_id"))
+    private List<Ticket> tickets;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{"
+                + "id=" + id
+                + ", tickets=" + tickets
+                + ", user=" + user
+                + '}';
+    }
+}
