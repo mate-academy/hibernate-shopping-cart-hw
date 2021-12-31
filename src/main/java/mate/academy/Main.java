2dcd30d0a850c5f7397ccf6f0b76861425087469
farfardesu <savavlad003@gmail.com>
2021-12-31 11:47:18 +0200

    minor changes were made

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -84,9 +84,9 @@ public class Main {
                 (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
         shoppingCartService.addSession(yesterdayMovieSession, testUserTwo);
         shoppingCartService.addSession(tomorrowMovieSession, testUserOne);
-        //shoppingCartService.clear(shoppingCartService.getByUser(testUserOne));
+        shoppingCartService.clear(shoppingCartService.getByUser(testUserOne));
         System.out.println(shoppingCartService.getByUser(testUserOne));
         System.out.println(shoppingCartService.getByUser(testUserTwo));
-
+        System.out.println(shoppingCartService.getByUser(testUserTwo).getTickets());
     }
 }
