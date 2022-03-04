c15db0f138c26f51e47e7520c99f0dd69bd04276
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-03-04 18:20:11 +0200

    Fix Ticket

--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -15,8 +15,7 @@ public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne(fetch = FetchType.LAZY,
-            cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY)
     private MovieSession movieSession;
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
