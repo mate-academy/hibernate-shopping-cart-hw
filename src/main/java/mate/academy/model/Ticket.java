6d50b62b9112c2d0bedb26b23e5b67c97ad84f7e
Andriy_Poluhovic <chimbar@ukr.net>
2022-03-24 18:00:59 +0200

    Was created classes Ticket and ShoppingCart

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,48 @@
+package mate.academy.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_session_id")
+    private MovieSession movieSession;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{"
+                + "id=" + id
+                + ", user=" + user
+                + ", movieSession=" + movieSession + '}';
+    }
+}
