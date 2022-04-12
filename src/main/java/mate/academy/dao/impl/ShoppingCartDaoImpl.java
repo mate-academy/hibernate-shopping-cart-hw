bde631f1a3194b44712e92637861038dc02bbcb1
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-04-12 14:48:06 +0300

    Fixed after student review

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -39,13 +39,13 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
     @Override
     public Optional<ShoppingCart> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<ShoppingCart> query = session.createQuery("FROM ShoppingCart sc "
-                    + "LEFT JOIN FETCH sc.tickets t "
-                    + "LEFT JOIN FETCH sc.user "
-                    + "LEFT JOIN FETCH t.movieSession ms "
-                    + "LEFT JOIN FETCH ms.movie "
-                    + "LEFT JOIN FETCH ms.cinemaHall "
-                    + "WHERE sc.user = :user", ShoppingCart.class);
+            Query<ShoppingCart> query = session.createQuery("from ShoppingCart sc "
+                    + "left join fetch sc.tickets t "
+                    + "left join fetch sc.user "
+                    + "left join fetch t.movieSession ms "
+                    + "left join fetch ms.movie "
+                    + "left join fetch ms.cinemaHall "
+                    + "where sc.user = :user", ShoppingCart.class);
             query.setParameter("user", user);
             return query.uniqueResultOptional();
         } catch (Exception e) {
