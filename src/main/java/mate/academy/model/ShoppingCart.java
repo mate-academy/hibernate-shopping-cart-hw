ee3832eadbf89208c12c6f12d7f1beb43658ca45
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-04-12 15:49:14 +0300

    Fixed after mentor review

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -9,8 +9,10 @@ import javax.persistence.JoinTable;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
     private Long id;
