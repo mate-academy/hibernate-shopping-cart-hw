8b7746b5381e2eb90609f4e28b9065e1daeb383d
vladsysa <vladsysa@mail.ru>
2022-04-12 13:15:10 +0200

    commit create hibernate-shopping-cart

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,16 +2,26 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.ShoppingCartService;
 
 public class Main {
     public static void main(String[] args) {
-        MovieService movieService = null;
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector
+                .getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
@@ -21,13 +31,13 @@ public class Main {
 
         CinemaHall firstCinemaHall = new CinemaHall();
         firstCinemaHall.setCapacity(100);
-        firstCinemaHall.setDescription("first hall with capacity 100");
-
+        firstCinemaHall.setDescription("capacity 100");
         CinemaHall secondCinemaHall = new CinemaHall();
         secondCinemaHall.setCapacity(200);
-        secondCinemaHall.setDescription("second hall with capacity 200");
+        secondCinemaHall.setDescription("capacity 200");
 
-        CinemaHallService cinemaHallService = null;
+        CinemaHallService cinemaHallService = (CinemaHallService) injector
+                .getInstance(CinemaHallService.class);
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
 
@@ -44,12 +54,47 @@ public class Main {
         yesterdayMovieSession.setMovie(fastAndFurious);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-        MovieSessionService movieSessionService = null;
+        MovieSessionService movieSessionService = (MovieSessionService) injector
+                .getInstance(MovieSessionService.class);
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                 fastAndFurious.getId(), LocalDate.now()));
+
+        AuthenticationService authenticationService = (AuthenticationService) injector
+                .getInstance(AuthenticationService.class);
+        User bambini;
+        try {
+            Ticket firstTicket = new Ticket();
+            authenticationService.register("bambini@gmail.com", "1234");
+            bambini = authenticationService.login("bambini@gmail.com", "1234");
+            firstTicket.setUser(bambini);
+            System.out.println("Added ticket: " + firstTicket + " to DB");
+        } catch (RegistrationException | AuthenticationException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+        ShoppingCartService shoppingCartService = (ShoppingCartService) injector
+                .getInstance(ShoppingCartService.class);
+        System.out.println("Registering new shopping cart for user: "
+                + bambini);
+        shoppingCartService.registerNewShoppingCart(bambini);
+
+        System.out.println("Adding new ticket with movie session: "
+                + tomorrowMovieSession);
+        shoppingCartService.addSession(tomorrowMovieSession, bambini);
+
+        System.out.println("Getting shopping cart for user: " + bambini);
+        ShoppingCart bambiniCart = shoppingCartService.getByUser(bambini);
+        System.out.println(bambiniCart);
+
+        System.out.println("Clearing shopping cart");
+        shoppingCartService.clear(bambiniCart);
+
+        System.out.println("Getting shopping cart for user: " + bambini);
+        bambiniCart = shoppingCartService.getByUser(bambini);
+        System.out.println(bambiniCart);
     }
 }
