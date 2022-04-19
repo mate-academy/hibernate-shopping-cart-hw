b3ff5ef01ce59f5686b591aad68cd5819e13d2c9
Viktor Stadnik <stadhik.victor@gmail.com>
2022-04-19 14:06:30 +0300

    The shopping cart was implemented

--- src/main/java/mate/academy/util/HashUtil.java
+++ src/main/java/mate/academy/util/HashUtil.java
@@ -6,6 +6,7 @@ import java.security.SecureRandom;
 
 public class HashUtil {
     private static final String HASH_ALGORITHM = "SHA-512";
+    private static final int NUMBER_SALT = 16;
 
     private HashUtil() {
     }
@@ -21,13 +22,14 @@ public class HashUtil {
             }
             return hashedPassword.toString();
         } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Can't hash password!", e);
+            throw new RuntimeException("Couldn't create hash using"
+                    + HASH_ALGORITHM + "algorithm", e);
         }
     }
 
     public static byte[] getSalt() {
         SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
+        byte[] salt = new byte[NUMBER_SALT];
         random.nextBytes(salt);
         return salt;
     }
