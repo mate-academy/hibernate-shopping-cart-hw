42896c4d6751181804a6e169a5e1ebb1c824647b
Valera Lemberov <valera.lemberov1993@gmail.com>
2022-04-23 12:48:24 +0300

    Implemented service and dao for cart and ticket.

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,61 @@
+package mate.academy.model;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shopping_cart")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Ticket> tickets;
+    @OneToOne
+    @MapsId
+    private User user;
+
+    public ShoppingCart() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{"
+                + "id=" + id
+                + ", tickets=" + tickets
+                + ", user_email=" + user.getEmail()
+                + '}';
+    }
+}
