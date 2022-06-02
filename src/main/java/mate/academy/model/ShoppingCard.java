24ccd44da292e4b1df109b2c55ff969e4978e920
Angela1608 <89303267+Angela1608@users.noreply.github.com>
2022-06-02 15:15:45 +0300

    refactoring

--- src/main/java/mate/academy/model/ShoppingCard.java
+++ src/main/java/mate/academy/model/ShoppingCard.java
@@ -2,12 +2,11 @@ package mate.academy.model;
 
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -20,7 +19,7 @@ public class ShoppingCard {
     @OneToMany
     @JoinColumn(name = "ticket_id")
     private List<Ticket> tickets;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToMany
     @JoinColumn(name = "user_id")
     private User user;
 
