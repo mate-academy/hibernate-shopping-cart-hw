113274dfbf2633a17f1f1ff226bcdcde739eecd5
OLEH VIESHAIEV <sred_a@i.ua>
2022-06-07 13:34:29 +0300

    fixed mistakes after mentor's review

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -40,7 +40,7 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
     public Optional<ShoppingCart> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<ShoppingCart> query = session.createQuery("FROM ShoppingCart sc "
-                    + "LEFT JOIN FETCH sc.tickets t "
+                    + "LEFT JOIN FETCH sc.tickets t LEFT JOIN FETCH sc.user "
                     + "LEFT JOIN FETCH t.movieSession ms LEFT JOIN FETCH ms.movie "
                     + "LEFT JOIN FETCH ms.cinemaHall WHERE sc.user =: user", ShoppingCart.class);
             query.setParameter("user", user);
