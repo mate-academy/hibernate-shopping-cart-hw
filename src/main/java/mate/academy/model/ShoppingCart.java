5e2a6726b966cf9c4e597ce53e0ce35df0d1d0ff
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-06-07 15:54:35 +0300

    requested changes

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -3,6 +3,8 @@ package mate.academy.model;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -14,6 +16,9 @@ public class ShoppingCart {
     @Id
     private Long id;
     @OneToMany
+    @JoinTable(name = "shopping_carts_tickets",
+            joinColumns = @JoinColumn(name = "shopping_cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
     @OneToOne
     @MapsId
@@ -48,6 +53,8 @@ public class ShoppingCart {
 
     @Override
     public String toString() {
-        return "ShoppingCart{" + "id=" + id + ", tickets=" + tickets + ", user=" + user + '}';
+        return "ShoppingCart{" + "id=" + id
+                + ", tickets=" + tickets
+                + ", user=" + user + '}';
     }
 }
