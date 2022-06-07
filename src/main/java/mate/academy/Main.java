10a64dac1816144d2086e45a98a83b02776fcf13
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-06-07 13:29:03 +0300

    added empty line

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,7 +2,6 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import mate.academy.dao.TicketDao;
 import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
@@ -14,7 +13,6 @@ import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
 import mate.academy.service.ShoppingCartService;
-import mate.academy.service.UserService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
@@ -28,9 +26,6 @@ public class Main {
             = (AuthenticationService) injector.getInstance(AuthenticationService.class);
     private static final ShoppingCartService shoppingCartService
             = (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
-    private static final UserService userService
-            = (UserService) injector.getInstance(UserService.class);
-    private static final TicketDao ticketDao = (TicketDao) injector.getInstance(TicketDao.class);
 
     public static void main(String[] args) {
         Movie fastAndFurious = new Movie("Fast and Furious");
--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -39,15 +39,16 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
     public Optional<ShoppingCart> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<ShoppingCart> query = session.createQuery(" FROM ShoppingCart sc "
-                    + "LEFT JOIN FETCH sc.tickets t "
-                    + "LEFT JOIN FETCH t.movieSession ms "
-                    + "LEFT JOIN FETCH ms.movie "
-                    + "LEFT JOIN FETCH ms.cinemaHall "
-                    + "WHERE sc.user = :user", ShoppingCart.class);
+                    + " LEFT JOIN FETCH sc.user"
+                    + " LEFT JOIN FETCH sc.tickets t "
+                    + " LEFT JOIN FETCH t.movieSession ms "
+                    + " LEFT JOIN FETCH ms.movie "
+                    + " LEFT JOIN FETCH ms.cinemaHall "
+                    + " WHERE sc.user = :user", ShoppingCart.class);
             query.setParameter("user", user);
             return query.uniqueResultOptional();
         } catch (Exception e) {
-            throw new DataProcessingException("Can't find user by user: " + user, e);
+            throw new DataProcessingException("Can't find user by card: " + user, e);
         }
     }
 
