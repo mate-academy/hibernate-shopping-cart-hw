f42a71e47eddaf298a32e4a8bc6ff30c40f6cb76
Nikita Semisoshenko <104957547+NikitaSemisoshenko@users.noreply.github.com>
2022-07-10 18:19:08 +0300

    was implemented

---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,55 @@
+package mate.academy.service.impl;
+
+import java.util.Collections;
+import java.util.List;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.exception.ShoppingCartException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private TicketDao ticketDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        Ticket ticket = new Ticket();
+        ticket.setUser(user);
+        ticket.setMovieSession(movieSession);
+        Ticket ticketFromDb = ticketDao.add(ticket);
+        ShoppingCart cart = shoppingCartDao.getByUser(user).orElseThrow(() ->
+                new ShoppingCartException("This user is don't have a shopping cart"));
+        List<Ticket> ticketsOfCurrentUser = cart.getTickets();
+        ticketsOfCurrentUser.add(ticketFromDb);
+        cart.setTickets(ticketsOfCurrentUser);
+        shoppingCartDao.update(cart);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return shoppingCartDao.getByUser(user).orElseThrow(() ->
+                        new ShoppingCartException("This user don't have shopping cart"));
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        ShoppingCart cart = new ShoppingCart();
+        cart.setUser(user);
+        shoppingCartDao.add(cart);
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.setTickets(Collections.emptyList());
+        shoppingCartDao.update(shoppingCart);
+    }
+}
