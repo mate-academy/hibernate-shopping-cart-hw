77788f270beac271caf2286ddc6e252f6f6aa110
Igor <ZIM20093@gmail.com>
2022-07-14 10:04:04 +0300

    remove @GeneratedValue from ShoppingCart

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -18,7 +18,6 @@ import javax.persistence.Table;
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
