8c337062a55ff526821e2f30cd793c47bbc58db0
Maksym Karamavrov <makcumkaka@gmail.com>
2022-07-26 17:07:01 +0300

    First commit. Application runs clear.

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,61 @@
+package mate.academy.model;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    private Long id;
+    @OneToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "shoppingCarts_tickets",
+            joinColumns = @JoinColumn(name = "shoppingCarts_id"),
+            inverseJoinColumns = @JoinColumn(name = "tickets_id"))
+    private List<Ticket> tickets;
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{"
+                + "id=" + id
+                + ", tickets=" + tickets
+                + ", user=" + user
+                + '}';
+    }
+}
