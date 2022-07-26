3dbab232387b35f94ded7a2319e743d592bf6e28
Pavlo99-gif <kurulchyk7355608@gmail.com>
2022-07-26 17:08:52 +0300

    change type exception

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.service.impl;
 
+import java.util.NoSuchElementException;
 import mate.academy.dao.ShoppingCartDao;
 import mate.academy.dao.TicketDao;
 import mate.academy.exception.DataProcessingException;
@@ -32,7 +33,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public ShoppingCart getByUser(User user) {
         return shoppingCartDao.getByUser(user).orElseThrow(() ->
-                new DataProcessingException("Can`t get shopping cart by user " + user));
+                new NoSuchElementException("Can`t get shopping cart by user " + user));
     }
     
     @Override
