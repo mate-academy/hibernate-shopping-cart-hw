67f4bcba73cb2761b09877b6ecb88a4711d49261
Mykola Yakymiv <mykola8426@gmail.com>
2022-07-27 18:20:50 +0300

    added fixes after review

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,7 +1,14 @@
 package mate.academy.model;
 
 import java.util.List;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class ShoppingCart {
