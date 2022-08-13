0b862988cf0b6dffda9aeee602d4089d9a591214
yaroslav <morozov.yrv@gmail.com>
2022-08-13 19:07:51 +0300

    added movieSession to toString method

--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -52,6 +52,7 @@ public class Ticket {
     public String toString() {
         return "Ticket{"
                 + "id=" + id
+                + ", movieSession=" + movieSession
                 + ", user=" + user
                 + '}';
     }
