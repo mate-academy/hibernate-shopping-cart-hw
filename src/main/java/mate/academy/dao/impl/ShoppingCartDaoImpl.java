131e9853bf95a9b8034ed5f396fd1e66fba37e8e
l_osyash <los.nazar24@gmail.com>
2022-09-01 21:39:03 +0300

    Hibernate.initialize() changed by join fetch in HQL query.

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -5,10 +5,8 @@ import mate.academy.dao.ShoppingCartDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.ShoppingCart;
-import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.util.HibernateUtil;
-import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
@@ -41,14 +39,12 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
     public Optional<ShoppingCart> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<ShoppingCart> query = session.createQuery("from ShoppingCart sc "
-                    + "left join fetch sc.tickets "
+                    + "left join fetch sc.tickets t "
+                    + "left join fetch t.movieSession ms "
+                    + "left join fetch ms.cinemaHall "
+                    + "left join fetch ms.movie "
                     + "where sc.id = :id", ShoppingCart.class);
             query.setParameter("id", user.getId());
-            ShoppingCart shoppingCart = query.getSingleResult();
-            for (Ticket ticket : shoppingCart.getTickets()) {
-                Hibernate.initialize(ticket.getMovieSession().getMovie());
-                Hibernate.initialize(ticket.getMovieSession().getCinemaHall());
-            }
             return query.uniqueResultOptional();
         } catch (Exception e) {
             throw new DataProcessingException("Can't get shopping cart for user: "
