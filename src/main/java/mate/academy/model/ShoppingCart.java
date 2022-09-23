8b08585ff36c6ea9f6a46b08efcfcfe7f3231175
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-09-23 13:42:50 +0300

    ShoppingCart and Ticket classes, dao, services are created

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,9 @@
+package mate.academy.model;
+
+import java.util.List;
+
+public class ShoppingCart {
+    private Long id;
+    private User user;
+    private List<Ticket> tickets;
+}
---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,7 @@
+package mate.academy.model;
+
+public class Ticket {
+    private Long id;
+    private MovieSession movieSession;
+    private User user;
+}
