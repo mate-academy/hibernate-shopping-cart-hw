ccc35e37453e0fc90fdd696710bfae850edf8c6e
Slava Verbanov <v.verbanov@gmail.com>
2022-09-26 17:22:31 +0200

    create models, dao and service layers for ShoppingCart, Tickets and User

---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,63 @@
+package mate.academy.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private TicketDao ticketDao;
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        Optional<ShoppingCart> shoppingCartOptional
+                = shoppingCartDao.getByUser(user);
+        if (shoppingCartOptional.isPresent()) {
+            Ticket ticket = new Ticket();
+            ticket.setUser(user);
+            ticket.setMovieSession(movieSession);
+            ticketDao.add(ticket);
+            ShoppingCart shoppingCart = shoppingCartOptional.get();
+            List<Ticket> tickets = shoppingCart.getTickets();
+            tickets.add(ticket);
+            shoppingCart.setTickets(tickets);
+            shoppingCartDao.update(shoppingCart);
+            return;
+        }
+        throw new RuntimeException("Can't find shoppingCart by user: " + user);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        Optional<ShoppingCart> shoppingCartOptional
+                = shoppingCartDao.getByUser(user);
+        return shoppingCartOptional.orElseThrow(
+                () -> new RuntimeException("Can't find shoppingCart by user: " + user));
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUser(user);
+        shoppingCart.setTickets(new ArrayList<>());
+        shoppingCartDao.add(shoppingCart);
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.setTickets(new ArrayList<>());
+        shoppingCartDao.update(shoppingCart);
+    }
+}
