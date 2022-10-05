6a4c3903b62dc2eb552c93f99ad481a03f78613e
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-05 16:26:42 +0300

    first commit. add required classes

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,54 @@
+package mate.academy.model;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToMany
+    //    @JoinColumn(name = "ticket_id")
+    private List<Ticket> tickets;
+    @OneToOne
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTicket() {
+        return tickets;
+    }
+
+    public void setTicket(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{id=" + id
+                + ", tickets=" + tickets
+                + ", user=" + user + '}';
+    }
+}
