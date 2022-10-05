516859937a98c6309097a5131adbc51c1ebd1d6d
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2022-10-05 23:19:37 +0300

    Added solution

---
+++ src/main/java/mate/academy/service/ShoppingCartService.java
@@ -0,0 +1,22 @@
+package mate.academy.service;
+
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+
+@Service
+public interface ShoppingCartService {
+    /**
+     * This method is responsible for adding a Ticket to the ShoppingCart
+     * @param movieSession contains the information required for the ticket
+     * @param user - the User who wants to buy the ticket for a specific movieSession
+     */
+    void addSession(MovieSession movieSession, User user);
+
+    ShoppingCart getByUser(User user);
+
+    void registerNewShoppingCart(User user);
+
+    void clear(ShoppingCart shoppingCart);
+}
