d16213bc191bfa638d55f7ad56b3a0c9a6b3b09f
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-06 00:34:26 +0300

    annotation in model ShoppingCart was fixed

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -25,7 +26,7 @@ public class ShoppingCart {
     private List<Ticket> tickets;
 
     @OneToOne
-    @JoinColumn(name = "user_id")
+    @MapsId
     private User user;
 
     public Long getId() {
