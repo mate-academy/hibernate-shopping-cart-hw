f4380217cbda28d555f4b32d5f9de8da052cf9c1
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-06 16:32:47 +0300

    some changes

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -4,6 +4,8 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -15,6 +17,7 @@ public class ShoppingCart {
     @Id
     private Long id;
     @OneToMany(fetch = FetchType.LAZY)
+    @JoinTable(joinColumns = @JoinColumn(name = "shopping_cart_user_id"))
     private List<Ticket> tickets;
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
