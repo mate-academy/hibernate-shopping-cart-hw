64251da55843b55cef8ac655cb349a1f8f35be50
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-06 16:11:29 +0300

    mreplacecart creationn

--- src/main/java/mate/academy/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -5,7 +5,6 @@ import mate.academy.dao.UserDao;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
-import mate.academy.service.ShoppingCartService;
 import mate.academy.service.UserService;
 import mate.academy.util.HashUtil;
 
@@ -13,15 +12,12 @@ import mate.academy.util.HashUtil;
 public class UserServiceImpl implements UserService {
     @Inject
     private UserDao userDao;
-    @Inject
-    private ShoppingCartService shoppingCartService;
 
     @Override
     public User add(User user) {
         user.setSalt(HashUtil.getSalt());
         user.setPassword(HashUtil.hashPassword(user.getPassword(), user.getSalt()));
         userDao.add(user);
-        shoppingCartService.registerNewShoppingCart(user);
         return user;
     }
 
