05aa595718aa7afc0bd36de7754965f374aa2a1d
fliarkovskyi <fliarkovskyi@gmail.com>
2022-10-06 18:05:30 +0300

    fix throws

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -21,7 +21,7 @@ import mate.academy.service.ShoppingCartService;
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
 
-    public static void main(String[] args) throws RegistrationException {
+    public static void main(String[] args) {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
