a9c7618d362b46b50bc76f96bf2218eee6497f7f
Danylo Frolov <frolovdanilik@gmail.com>
2022-10-06 16:13:56 +0300

    applied changes

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,13 +1,10 @@
 package mate.academy.model;
 
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.MapsId;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
     private Long id;
