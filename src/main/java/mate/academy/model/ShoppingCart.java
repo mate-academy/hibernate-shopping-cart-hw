0c1437fb4fa5b1a0ba032a76104c22604f8e7cf8
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-11-27 13:45:13 +0200

    Delete generated value for id field

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -16,7 +16,6 @@ import javax.persistence.Table;
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany(cascade = CascadeType.MERGE)
     private List<Ticket> tickets;
