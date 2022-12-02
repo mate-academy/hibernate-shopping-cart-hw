2fb078df6d94088bef0d75944aa6575858896a33
galenkoigor <galenkoigor@gmail.com>
2022-12-02 12:07:55 +0100

    Added Hibernates annotations to ShoppingCart class

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,11 +1,26 @@
 package mate.academy.model;
 
 import java.util.List;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "shopping_carts")
 public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "shopping_carts_tickets",
+    joinColumns = @JoinColumn(name = "shopping_cart_id"),
+    inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
+
+    public Long getId() {
+        return id;
+    }
 
     public ShoppingCart() {
     }
@@ -13,14 +28,6 @@ public class ShoppingCart {
     public ShoppingCart(User user, List<Ticket> tickets) {
         this.user = user;
         this.tickets = tickets;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public List<Ticket> getTickets() {
