c29babf2c15ad244a80d3df1b683c219509291ff
Pavlo.Polovyi <pasha32662@gmail.com>
2022-12-08 16:49:57 +0200

    Created models: ShoppingCart, Ticket; created ShoppingCartDao and TicketDao; created ShoppingCartService; tested Main.main.

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,83 @@
+package mate.academy.model;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_session_id")
+    private MovieSession movieSession;
+
+    public Ticket() {
+    }
+
+    public Ticket(User user, MovieSession movieSession) {
+        this.user = user;
+        this.movieSession = movieSession;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Ticket ticket = (Ticket) o;
+        return Objects.equals(id, ticket.id) && Objects.equals(user, ticket.user)
+                && Objects.equals(movieSession, ticket.movieSession);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, movieSession);
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{"
+                + "id=" + id
+                + ", user=" + user
+                + ", movieSession=" + movieSession
+                + '}';
+    }
+}
