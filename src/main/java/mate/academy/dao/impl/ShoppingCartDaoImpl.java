3859cbda562eb35d79e86a4169afcf4b73530433
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-12-08 21:39:44 +0200

    implemented Shopping Cart service/dao (created model), ticket (model), dao

---
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,73 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Fetch;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Root;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add shopping cart: "
+                    + shoppingCart + " to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<ShoppingCart> getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<ShoppingCart> query = criteriaBuilder.createQuery(ShoppingCart.class);
+            Root<ShoppingCart> root = query.from(ShoppingCart.class);
+            Fetch<Object, Object> tickets = root.fetch("tickets", JoinType.LEFT);
+            Fetch<Object, Object> movieSession = tickets.fetch("movieSession", JoinType.LEFT);
+            movieSession.fetch("movie", JoinType.LEFT);
+            movieSession.fetch("cinemaHall", JoinType.LEFT);
+            query.where(criteriaBuilder.equal(root.get("user"), user.getId()));
+            return session.createQuery(query).uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get shopping cart by user=" + user, e);
+        }
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.update(shoppingCart);
+            transaction.commit();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't merge shopping cart=" + shoppingCart, e);
+        }
+    }
+}
