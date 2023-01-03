b9c370d4eea7dcb37f7f0a47cc587a47d83bde50
DooMLeoN <levberezov01@gmail.com>
2023-01-03 12:30:49 +0200

    first commit

---
+++ src/main/java/mate/academy/dao/TicketsDao.java
@@ -0,0 +1,7 @@
+package mate.academy.dao;
+
+import mate.academy.model.Ticket;
+
+public interface TicketsDao {
+    Ticket add(Ticket ticket);
+}
---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,58 @@
+package mate.academy.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MovieSession movieSession;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private User user;
+
+    public Ticket() {
+    }
+
+    public Ticket(MovieSession movieSession, User user) {
+        this.movieSession = movieSession;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id=" + id +
+                ", movieSession=" + movieSession +
+                ", user=" + user +
+                '}';
+    }
+}
