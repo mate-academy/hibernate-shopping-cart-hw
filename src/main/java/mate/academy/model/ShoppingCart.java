d4b8fb61c54378b5e05de523889aa5e4345ca600
AndriySichniy <sichniy.andriy@gmail.com>
2023-01-12 16:52:11 +0200

    add Ticket and ShoppingCart models

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,34 @@
+package mate.academy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToMany
+    @JoinColumn(name = "ticket_id")
+    private List<Ticket> tickets;
+    @OneToOne
+    @MapsId
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
