b2b969b4a3409c73a6bda8627c82a0b56077b4da
Roman Denysiuk <r.v.denysiuk@gmail.com>
2023-01-13 16:03:20 +0200

    changes after review

--- src/main/java/mate/academy/dao/ShoppingCartDao.java
+++ src/main/java/mate/academy/dao/ShoppingCartDao.java
@@ -6,7 +6,6 @@ import mate.academy.model.ShoppingCart;
 import mate.academy.model.User;
 
 public interface ShoppingCartDao {
-
     ShoppingCart add(ShoppingCart shoppingCart);
 
     Optional<ShoppingCart> getByUser(User user);
--- src/main/java/mate/academy/dao/TicketDao.java
+++ src/main/java/mate/academy/dao/TicketDao.java
@@ -3,7 +3,5 @@ package mate.academy.dao;
 import mate.academy.model.Ticket;
 
 public interface TicketDao {
-
     Ticket add(Ticket ticket);
-
 }
