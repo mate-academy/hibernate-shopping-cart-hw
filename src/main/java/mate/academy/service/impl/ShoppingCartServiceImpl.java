36bae597adc44f5c3ded1b5eed1d8d62468f08dd
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2023-02-19 18:44:51 +0200

    Code improved

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -46,7 +46,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Override
     public void clear(ShoppingCart shoppingCart) {
-        shoppingCart.setTickets(new ArrayList<>());
+        shoppingCart.getTickets().clear();
         shoppingCartDao.update(shoppingCart);
     }
 }
