cc4e383dd54accb3af680d8e1d895c661011cbe9
YuriiDikalo <79626945+yurii-dikalo@users.noreply.github.com>
2023-02-20 12:35:15 +0200

    Update Ticket.java

--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -14,9 +14,9 @@ public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private MovieSession movieSession;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     public Long getId() {
