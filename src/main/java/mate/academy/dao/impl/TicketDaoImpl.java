f22ffda89546a66b6bcddc59df8ac9160ed40bd4
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2023-02-25 13:06:00 +0200

    Created TicketDao.class,TicketDaoImpl.class,ShoppingCartDao.class,ShoppingCartDaoImpl.class

---
+++ src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
@@ -0,0 +1,10 @@
+package mate.academy.dao.impl;
+
+import mate.academy.model.Ticket;
+
+public class TicketDaoImpl implements mate.academy.dao.TicketDao {
+    @Override
+    public Ticket add(Ticket ticket) {
+        return null;
+    }
+}
