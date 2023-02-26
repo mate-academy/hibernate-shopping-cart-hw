ef7ee52f4ff6e93dbe271df78620816ab3a01657
Dima Senko <d.senko@i.ua>
2023-02-26 11:41:06 +0200

    commit 1

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -39,9 +39,12 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
     @Override
     public Optional<ShoppingCart> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<ShoppingCart> query = session.createQuery("FROM ShoppingCart sp "
-                    + "LEFT JOIN FETCH sp.tickets "
-                    + "WHERE sp.user = :user", ShoppingCart.class);
+            Query<ShoppingCart> query = session.createQuery("FROM ShoppingCart sc "
+                    + "LEFT JOIN FETCH sc.tickets t "
+                    + "LEFT JOIN FETCH t.movieSession ms "
+                    + "LEFT JOIN FETCH ms.movie "
+                    + "LEFT JOIN FETCH ms.cinemaHall "
+                    + "WHERE sc.user = :user", ShoppingCart.class);
             query.setParameter("user", user);
             return query.uniqueResultOptional();
         } catch (Exception e) {
