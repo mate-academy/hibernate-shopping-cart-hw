41fa0cd662d9806f7a54b69d2e606298d68014be
bogdanpokhilko <bogdanp1701@gmail.com>
2023-03-04 19:56:44 +0200

    HW is done. Waiting for the review. Thank you for advice. :)

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -2,6 +2,7 @@ package mate.academy.model;
 
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,10 +16,10 @@ public class MovieSession {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "movie_id")
     private Movie movie;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cinema_hall_id")
     private CinemaHall cinemaHall;
     private LocalDateTime showTime;
