4d8ee35c01b4c13e594c561ce648f46f091e037c
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2023-03-13 16:29:45 +0200

    added checkstyle fixes

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,7 +1,14 @@
 package mate.academy.model;
 
 import java.util.List;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "shopping_carts")
