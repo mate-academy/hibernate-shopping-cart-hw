36cb31743a85b607108e4f6695c4112d09f8876d
Serhii Shyian <serhii.shiyan@ukr.net>
2023-04-12 10:45:30 +0300

    1) Updated dependencies in pom.xml 2) Created hibernate.cfg.xml in resources package 3) Created ShoppingCart and Ticket classes in model package 4) Created ShoppingCartDao class in model package and implemented add(), getByUser(), update() methods 5) Created TicketDao class in model package and implemented add() method 6) In DataProcessingException added method that accepts String 7) Created ShoppingCartService class in service package and implemented addSession(), getByUser(), registerNewShoppingCart(), clear() methods 8) In User class added constructors with 2 parameters 9) In AuthenticationServiceImpl class in register() method realised creating user without setting parameters, and added registerNewShoppingCart() in logic 10) In MovieSession class added annotations @JoinColumn and @Column 11) In Main class created instances for MovieService, CinemaHallService, MovieSessionService, AuthenticationService, ShoppingCartService and tested all ShoppingCartService methods

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,16 +2,23 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import mate.academy.model.CinemaHall;
-import mate.academy.model.Movie;
-import mate.academy.model.MovieSession;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Injector;
+import mate.academy.model.*;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.ShoppingCartService;
 
 public class Main {
+    private static final Injector injector
+            = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        MovieService movieService = null;
+        MovieService movieService
+                = (MovieService) injector.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
@@ -27,7 +34,8 @@ public class Main {
         secondCinemaHall.setCapacity(200);
         secondCinemaHall.setDescription("second hall with capacity 200");
 
-        CinemaHallService cinemaHallService = null;
+        CinemaHallService cinemaHallService
+                = (CinemaHallService) injector.getInstance(CinemaHallService.class);
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
 
@@ -44,12 +52,57 @@ public class Main {
         yesterdayMovieSession.setMovie(fastAndFurious);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-        MovieSessionService movieSessionService = null;
+        MovieSessionService movieSessionService
+                = (MovieSessionService) injector.getInstance(MovieSessionService.class);
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                 fastAndFurious.getId(), LocalDate.now()));
+
+        AuthenticationService authenticationService
+                = (AuthenticationService) injector.getInstance(AuthenticationService.class);
+        User user;
+        try {
+            user = authenticationService.register(
+                    "test@mate.academy",
+                    "Mate2023");
+        } catch (RegistrationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        try {
+            user = authenticationService.login(
+                    "test@mate.academy",
+                    "Mate2023");
+        } catch (AuthenticationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        ShoppingCartService shoppingCartService
+                = (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
+        System.out.println("----------------------------");
+        System.out.println("Start testing ShoppingCartService.");
+
+        System.out.println("----------------------------");
+        System.out.println("AddSession method was called.");
+        shoppingCartService.addSession(yesterdayMovieSession, user);
+        shoppingCartService.addSession(tomorrowMovieSession, user);
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("GetByUser method was called.");
+        ShoppingCart userCart = shoppingCartService.getByUser(user);
+        System.out.println(userCart);
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("Clear method was called.");
+        shoppingCartService.clear(userCart);
+        shoppingCartService.getByUser(user);
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("All tests finished.");
     }
 }
---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,54 @@
+package mate.academy.service.impl;
+
+import java.util.Collections;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private TicketDao ticketDao;
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        Ticket ticket = new Ticket(
+                movieSession,
+                user);
+        ticketDao.add(ticket);
+
+        ShoppingCart shoppingCart = getByUser(user);
+        shoppingCart.getTickets().add(ticket);
+        shoppingCartDao.update(shoppingCart);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return shoppingCartDao.getByUser(user)
+                .orElseThrow(() ->
+                        new DataProcessingException("Didn't find shoppingCart for user " + user));
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        ShoppingCart shoppingCart = new ShoppingCart(
+                Collections.emptyList(),
+                user);
+        shoppingCartDao.add(shoppingCart);
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.setTickets(Collections.emptyList());
+        shoppingCartDao.update(shoppingCart);
+    }
+}
