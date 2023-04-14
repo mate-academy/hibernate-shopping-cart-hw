9eba368b773a3545dfc43a059133b24a58c39f46
Skrudj <ssinilov@gmail.com>
2023-04-14 22:43:22 +0300

    implemented ticket and shoppingCart models

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,4 +1,63 @@
 package mate.academy.model;
 
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shopping_carts")
 public class ShoppingCart {
+    @Id
+    private Long id;
+    @OneToMany
+    private List<Ticket> tickets;
+    @OneToOne
+    @MapsId
+    private User user;
+
+    public ShoppingCart() {
+    }
+
+    public ShoppingCart(Long id, List<Ticket> tickets, User user) {
+        this.id = id;
+        this.tickets = tickets;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{"
+                + "id=" + id + '\''
+                + ", tickets=" + tickets + '\''
+                + ", user=" + user + '\''
+                + '}';
+    }
 }
