59eeb07976195456493786f4e41510821714a19f
Denys Dominskyi <dominskyj@gmail.com>
2023-04-17 21:26:44 +0300

    Initial commit

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,45 @@
+package mate.academy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
+
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private MovieSession session;
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MovieSession getSession() {
+        return session;
+    }
+
+    public void setSession(MovieSession session) {
+        this.session = session;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
