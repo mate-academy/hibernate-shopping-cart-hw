4a128ac4a7bd4026ec89c0d85895babc75cb43ed
KosMarch <kosmarchenko98@gmail.com>
2023-04-19 21:15:29 +0300

    added ShoppingCartServiceImpl

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -1,30 +1,47 @@
 package mate.academy.service.impl;
 
+import java.util.Collections;
+import java.util.NoSuchElementException;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.MovieSession;
 import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.service.ShoppingCartService;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private TicketDao ticketDao;
+
     @Override
     public void addSession(MovieSession movieSession, User user) {
-
+        Ticket ticket = new Ticket();
+        ticket.setUser(user);
+        ticket.setMovieSession(movieSession);
+        ticketDao.add(ticket);
     }
 
     @Override
     public ShoppingCart getByUser(User user) {
-        return null;
+        return shoppingCartDao.getByUser(user).orElseThrow(() ->
+                new NoSuchElementException("Can't get user: " + user));
     }
 
     @Override
     public void registerNewShoppingCart(User user) {
-
+        ShoppingCart newShoppingCart = new ShoppingCart();
+        newShoppingCart.setUser(user);
+        shoppingCartDao.add(newShoppingCart);
     }
 
     @Override
     public void clear(ShoppingCart shoppingCart) {
-
+        shoppingCart.setTickets(Collections.emptyList());
     }
 }
