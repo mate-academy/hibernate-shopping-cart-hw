8f602ab34118872f9b01e7e1271eacfb786f3fb1
Oleksandr Krasnov <redman2775@gmail.com>
2023-04-20 23:34:24 +0300

    1. ShoppingCart & Ticket models - created; 2. ShoppingCartDao & TicketDao classes - created & their methods - implemented; 3. ShoppingCartService with methods implementations - created; 4. Added methods in main - tested; 5. Maven checkstyle - BUILD SUCCESS;

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,17 +2,37 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.ShoppingCartService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final String VALID_EMAIL = "user@com";
+    private static final String PASS = "12345678";
+    private static final Injector injector =
+            Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+    private static final CinemaHallService cinemaHallService =
+            (CinemaHallService) injector.getInstance(CinemaHallService.class);
+    private static final MovieSessionService movieSessionService =
+            (MovieSessionService) injector.getInstance(MovieSessionService.class);
+    private static final AuthenticationService authService =
+            (AuthenticationService) injector.getInstance(AuthenticationService.class);
+    private static final ShoppingCartService shopCartService =
+            (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
 
+    public static void main(String[] args) {
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
@@ -27,7 +47,6 @@ public class Main {
         secondCinemaHall.setCapacity(200);
         secondCinemaHall.setDescription("second hall with capacity 200");
 
-        CinemaHallService cinemaHallService = null;
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
 
@@ -44,12 +63,35 @@ public class Main {
         yesterdayMovieSession.setMovie(fastAndFurious);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-        MovieSessionService movieSessionService = null;
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                 fastAndFurious.getId(), LocalDate.now()));
+
+        System.out.println("----------------------------new added functions--------------------------------");
+
+        try {
+            authService.register(VALID_EMAIL, PASS);
+            System.out.println("User registered");
+        } catch (RegistrationException e) {
+            System.out.println("Couldn't register user");
+        }
+
+        User user = null;
+        try {
+            user = authService.login(VALID_EMAIL, PASS);
+            System.out.println("User logged");
+        } catch (AuthenticationException e) {
+            System.out.println("Couldn't login user");
+        }
+        System.out.println(user);
+
+        shopCartService.addSession(movieSessionService.get(user.getId()), user);
+        ShoppingCart shoppingCart = shopCartService.getByUser(user);
+        System.out.println(shoppingCart + "added");
+        shopCartService.clear(shoppingCart);
+        System.out.println(shoppingCart + "cleared");
     }
 }
