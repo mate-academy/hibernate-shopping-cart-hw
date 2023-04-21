d9a9c97fe8a32deed982431145614277a4313d64
potrap <potrapmax@gmail.com>
2023-04-21 15:25:27 +0300

    fixed

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,12 +1,7 @@
 package mate.academy.model;
 
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.MapsId;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "shopping_carts")
@@ -14,6 +9,7 @@ public class ShoppingCart {
     @Id
     private Long id;
     @OneToMany
+    @JoinColumn(name = "tickets")
     private List<Ticket> tickets;
     @OneToOne
     @MapsId
