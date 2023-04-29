e919542e9b6106b9b46e3b61f819b032364b6bc9
Yevhenii Kilovyi <118058456+YevheniiKilovyi@users.noreply.github.com>
2023-04-29 14:14:27 +0300

    fixed relations

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -16,6 +17,9 @@ public class ShoppingCart {
     @Id
     private Long id;
     @OneToMany
+    @JoinTable(name = "shopping_carts_tickets",
+            joinColumns = @JoinColumn(name = "shopping_cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
     @OneToOne
     @MapsId
