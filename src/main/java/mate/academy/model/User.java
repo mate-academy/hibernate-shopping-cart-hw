1df3520182667be197c9d9c6019a09ee7b450046
Gamemod13 <igorfetov1997@gmail.com>
2023-07-07 11:50:40 +0300

    Remove redundant Constructor with 2 parameters

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -21,11 +21,6 @@ public class User {
     public User() {
     }
 
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
     public Long getId() {
         return id;
     }
