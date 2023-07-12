d6f831cea99c71f4e026d76ba1cd9e3c2c297149
Igor Maksimov <maksigors@gmail.com>
2023-07-12 18:01:13 +0300

    build 1.3

--- src/main/java/mate/academy/security/AuthenticationService.java
+++ src/main/java/mate/academy/security/AuthenticationService.java
@@ -2,7 +2,6 @@ package mate.academy.security;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
-import mate.academy.lib.Service;
 import mate.academy.model.User;
 
 public interface AuthenticationService {
