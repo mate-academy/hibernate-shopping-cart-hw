51df1f3100733a14d6973e570e46782aac56faac
Igor Maksimov <maksigors@gmail.com>
2023-07-12 13:32:58 +0300

    build 1.0

--- src/main/java/mate/academy/security/AuthenticationService.java
+++ src/main/java/mate/academy/security/AuthenticationService.java
@@ -2,8 +2,10 @@ package mate.academy.security;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Service;
 import mate.academy.model.User;
 
+@Service
 public interface AuthenticationService {
     User login(String email, String password) throws AuthenticationException;
 
