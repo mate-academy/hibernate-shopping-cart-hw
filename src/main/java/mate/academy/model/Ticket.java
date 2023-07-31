a8e31ae928e38c8b99af3a684d0bfb6709a86932
Igor Parovenko <igorparovenko@yahoo.com>
2023-07-31 19:54:19 +0200

    ShoppingCart and Ticket classes created

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,51 @@
+package mate.academy.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MovieSession movieSession;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    public Ticket() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id=" + id +
+                ", movieSession=" + movieSession +
+                ", user=" + user +
+                '}';
+    }
+}
