c9b14a03bbdd0c400c7a72160a3a6066b2ff1ac0
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-08-06 13:13:28 +0300

    added fetch joins in getByUser method

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -42,7 +42,10 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
             session = HibernateUtil.getSessionFactory().openSession();
             Query<ShoppingCart> query = session
                     .createQuery("from ShoppingCart sc "
-                                    + "LEFT JOIN FETCH sc.tickets "
+                                    + "LEFT JOIN FETCH sc.tickets t "
+                                    + "LEFT JOIN FETCH t.movieSession ms "
+                                    + "LEFT JOIN FETCH ms.movie "
+                                    + "LEFT JOIN FETCH ms.cinemaHall "
                                     + "where sc.user=:inputUser",
                             ShoppingCart.class);
             query.setParameter("inputUser", user);
