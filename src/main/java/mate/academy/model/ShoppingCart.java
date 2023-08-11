3ed91887d7f3672bd2d6761cc6be6ea44d6076fd
heilxwotan1 <noobdima228@gmail.com>
2023-08-11 09:15:42 +0300

    fixed mistake from review

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -2,8 +2,6 @@ package mate.academy.model;
 
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
@@ -12,7 +10,6 @@ import javax.persistence.OneToOne;
 @Entity
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToOne
     @MapsId
