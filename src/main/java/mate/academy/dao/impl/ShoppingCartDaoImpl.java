95db36af4a3b86bfe3b0d73cdfaafcaebae2d785
literandltx <romanskovronn@gmail.com>
2023-08-20 18:53:11 +0300

    fix getByUser ShoppingCartDao

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -9,6 +9,7 @@ import mate.academy.model.User;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 @Dao
 public class ShoppingCartDaoImpl implements ShoppingCartDao {
@@ -38,10 +39,16 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
     @Override
     public Optional<ShoppingCart> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            return Optional.ofNullable(session.get(ShoppingCart.class, user.getId()));
+            Query<ShoppingCart> getCartByUserQuery = session.createQuery("FROM ShoppingCart sc "
+                    + "LEFT JOIN FETCH sc.tickets t "
+                    + "LEFT JOIN FETCH t.movieSession ms "
+                    + "LEFT JOIN FETCH ms.movie "
+                    + "LEFT JOIN FETCH ms.cinemaHall "
+                    + "WHERE sc.user = :user ", ShoppingCart.class);
+            getCartByUserQuery.setParameter("user", user);
+            return getCartByUserQuery.uniqueResultOptional();
         } catch (Exception e) {
-            throw new DataProcessingException(
-                    "Can't get a shoppingCart by user id: " + user.getId(), e);
+            throw new DataProcessingException("Can't get shopping cart for user: " + user, e);
         }
     }
 
