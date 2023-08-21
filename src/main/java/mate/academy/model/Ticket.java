5f412f0caa756f8fbd8ac2be9f22dbb71bb101b8
Denis Balako <denisarius2000s@gmail.com>
2023-08-21 12:50:13 +0300

    removed unnecessary comments and changed table names

--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -9,7 +9,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tickets")
+@Table(name = "ticket")
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
