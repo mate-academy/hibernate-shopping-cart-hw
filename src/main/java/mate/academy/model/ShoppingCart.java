685c8cf96a2b882d1e2536e0c3fe81f562e3a11f
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-08-23 22:33:48 +0300

    Added line and deleted redundant GeneratedValue

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -16,7 +16,6 @@ import java.util.List;
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
     @JoinColumn(name = "shopping_cart_id")
