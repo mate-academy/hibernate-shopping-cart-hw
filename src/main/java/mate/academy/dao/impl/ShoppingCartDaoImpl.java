4b840026589238765f2d6ec26ece612023ebe40a
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-08-24 16:48:11 +0300

    implemented ShopCartService v 1.0

---
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,62 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert shoppingCart" + shoppingCart, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<ShoppingCart> getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<ShoppingCart> findByUserQuery = session.createQuery("from ShoppingCart "
+                            + "WHERE user_id = :id",
+                    ShoppingCart.class);
+            findByUserQuery.setParameter("id", user.getId());
+            return findByUserQuery.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t find shopping cart with user: " + user, e);
+        }
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.merge(shoppingCart);
+            transaction.commit();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't update shopping cart: " + shoppingCart, e);
+        }
+    }
+}
