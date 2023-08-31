73edf9e7df54eedbf7c0248607fda9969fd291c4
Mykyta Zuber <nikitazuber@gmail.com>
2023-08-31 17:12:20 +0300

    fixed code

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -20,7 +20,7 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
         try {
             session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            session.save(shoppingCart);
+            session.persist(shoppingCart);
             transaction.commit();
             return shoppingCart;
         } catch (Exception e) {
