9fbdd9fccc0c4f3beb13486abcac7103a9daac08
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-09-02 14:21:15 +0300

    fixed some errors

--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -10,7 +10,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tickets")
+@Table(name = "ticket")
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
