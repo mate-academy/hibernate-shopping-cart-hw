e14f99e1c037a52f9a4b1f02eabf7940d220d061
ivanovalyub56 <140909880+ivanovalyub56@users.noreply.github.com>
2023-09-09 11:01:47 +0300

    Solution of this task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,16 +2,36 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.dao.TicketDao;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.ShoppingCartService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
 
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.academy");
+    private static ShoppingCartService shoppingCartService = (ShoppingCartService) injector
+            .getInstance(ShoppingCartService.class);
+    private static MovieService movieService = (MovieService) injector
+            .getInstance(MovieService.class);
+    private static CinemaHallService cinemaHallService = (CinemaHallService) injector
+            .getInstance(CinemaHallService.class);
+    private static MovieSessionService movieSessionService = (MovieSessionService) injector
+            .getInstance(MovieSessionService.class);
+    private static UserService userService = (UserService) injector
+            .getInstance(UserService.class);
+    private static TicketDao ticketDao = (TicketDao) injector
+            .getInstance(TicketDao.class);
+
     public static void main(String[] args) {
-        MovieService movieService = null;
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
@@ -27,7 +47,6 @@ public class Main {
         secondCinemaHall.setCapacity(200);
         secondCinemaHall.setDescription("second hall with capacity 200");
 
-        CinemaHallService cinemaHallService = null;
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
 
@@ -37,19 +56,40 @@ public class Main {
         MovieSession tomorrowMovieSession = new MovieSession();
         tomorrowMovieSession.setCinemaHall(firstCinemaHall);
         tomorrowMovieSession.setMovie(fastAndFurious);
-        tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1L));
+        tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1));
 
         MovieSession yesterdayMovieSession = new MovieSession();
         yesterdayMovieSession.setCinemaHall(firstCinemaHall);
         yesterdayMovieSession.setMovie(fastAndFurious);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
-
-        MovieSessionService movieSessionService = null;
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                 fastAndFurious.getId(), LocalDate.now()));
+
+        User alice = new User();
+        alice.setEmail("alice@gmail.com");
+        alice.setPassword("qwerty");
+        alice.setSalt(HashUtil.getSalt());
+        userService.add(alice);
+        User bob = new User();
+        bob.setEmail("bob@gmail.com");
+        bob.setPassword("q1w2e3r4t5");
+        bob.setSalt(HashUtil.getSalt());
+        userService.add(bob);
+
+        Ticket ticket = new Ticket();
+        ticket.setUser(bob);
+        ticket.setMovieSession(yesterdayMovieSession);
+        System.out.println(ticketDao.add(ticket));
+
+        shoppingCartService.registerNewShoppingCart(alice);
+        shoppingCartService.addSession(yesterdayMovieSession, alice);
+        shoppingCartService.addSession(tomorrowMovieSession, alice);
+        System.out.println(shoppingCartService.getByUser(alice));
+        shoppingCartService.clear(shoppingCartService.getByUser(alice));
+        System.out.println(shoppingCartService.getByUser(alice));
     }
 }
