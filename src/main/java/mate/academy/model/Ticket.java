84696489ff23e7267b4bcfbc3eb207770310a63e
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-10-10 20:18:22 +0300

    added ShoppingCart, Ticket mode, dao and service

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,53 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    private Long id;
+
+    @OneToOne
+    private MovieSession movieSession;
+
+    @ManyToOne
+    private User user;
+
+    public Ticket() {
+    }
+
+    public Ticket(MovieSession movieSession, User user) {
+        this.movieSession = movieSession;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
