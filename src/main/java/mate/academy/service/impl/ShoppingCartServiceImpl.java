561a8812de548d6c66903a304a70de4ead50f3fe
yurlis <yuri.lisovski@gmail.com>
2023-10-28 14:48:25 +0300

    Replaced the problem with LazyInitializationException from annotation EAGER to solution by JOIN FETCH in getByUser method. Add an output to the console for ShoppingCart testing tn Main method. Use Inject for AuthenticationServiceImpl for shoppingCartService. Resolve a bug in findAvailableSessions. Use another way adding new Ticket in addSession.

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -1,10 +1,8 @@
 package mate.academy.service.impl;
 
-import java.util.Collections;
 import java.util.List;
 import mate.academy.dao.ShoppingCartDao;
 import mate.academy.dao.TicketDao;
-import mate.academy.dao.impl.TicketDaoImpl;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.MovieSession;
