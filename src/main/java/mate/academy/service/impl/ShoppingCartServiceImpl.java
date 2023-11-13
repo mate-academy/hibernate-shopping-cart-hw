3ce54e15b6ec163e424ec17d07a50ef50cc04583
testSouth <southparaplan@gmail.com>
2023-11-13 17:40:52 +0200

    added interaface ShoppingCartService and implementation

---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,46 @@
+package mate.academy.service.impl;
+
+import jakarta.persistence.EntityNotFoundException;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+
+import java.util.ArrayList;
+
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    TicketDao ticketDao;
+    ShoppingCartDao shoppingCartDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        Ticket newTicket = new Ticket();
+        newTicket.setUser(user);
+        newTicket.setMovieSession(movieSession);
+        ShoppingCart shoppingCart = shoppingCartDao.getByUser(user).orElseThrow();
+        shoppingCart.getTicketList().add(ticketDao.add(newTicket));
+        shoppingCartDao.update(shoppingCart);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return shoppingCartDao.getByUser(user).orElseThrow(()
+                -> new EntityNotFoundException("Can not get User: " + user));
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUser(user);
+        shoppingCartDao.add(shoppingCart);
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.setTicketList(new ArrayList<>());
+        shoppingCartDao.update(shoppingCart);
+    }
+}
