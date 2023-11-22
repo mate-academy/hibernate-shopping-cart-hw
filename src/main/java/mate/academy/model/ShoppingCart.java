e320dfac8b13a5d6692f9a1882adb496bdc88353
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-11-22 20:43:37 +0200

    created new dao classes, movels and SCService

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,50 @@
+package mate.academy.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToMany
+    private List<Ticket> tickets;
+    @OneToOne
+    @MapsId
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{"
+                + "id=" + id
+                + ", tickets=" + tickets
+                + ", user=" + user
+                + '}';
+    }
+}
