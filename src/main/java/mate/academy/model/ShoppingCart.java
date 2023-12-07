819b5116524316ba875657033663d7b29c47e9b7
yevhenreuta <r3utaa@gmail.com>
2023-12-07 12:59:41 +0200

    implemented shopping cart and ticket

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,46 @@
+package mate.academy.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "shopping_cart")
+public class ShoppingCart {
+    @Id
+    private Long id;
+    @OneToMany(cascade = CascadeType.REMOVE)
+    private List<Ticket> tickets;
+    @OneToOne
+    @MapsId
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
