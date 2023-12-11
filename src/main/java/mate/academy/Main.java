dab6265dc23585b0dd18ce60193096fbdf6969ba
Artem <artempharm83@gmail.com>
2023-12-11 17:24:21 +0200

    created ShoppingCart, Ticket models, created ShoppingCartDao,TicketDao, created ShoppingCartService

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,16 +2,24 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.ShoppingCartService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) throws AuthenticationException {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
@@ -51,5 +59,22 @@ public class Main {
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                 fastAndFurious.getId(), LocalDate.now()));
+
+        AuthenticationService authenticationService = (AuthenticationService) injector
+                .getInstance(AuthenticationService.class);
+
+        User testUser;
+        try {
+            testUser = authenticationService.register("test1@gmail.com","test1");
+        } catch (RegistrationException e) {
+            throw new RuntimeException("Can't create user", e);
+        }
+
+        ShoppingCartService shoppingCartService = (ShoppingCartService) injector
+                .getInstance(ShoppingCartService.class);
+        shoppingCartService.addSession(tomorrowMovieSession, testUser);
+        System.out.println(shoppingCartService.getByUser(testUser));
+        shoppingCartService.clear(shoppingCartService.getByUser(testUser));
+        System.out.println(shoppingCartService.getByUser(testUser));
     }
 }
---
+++ src/main/java/mate/academy/service/impl/ShoppingCarServiceImpl.java
@@ -0,0 +1,51 @@
+package mate.academy.service.impl;
+
+import java.util.Collections;
+import java.util.Optional;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class ShoppingCarServiceImpl implements ShoppingCartService {
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private TicketDao ticketDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        Ticket ticket = new Ticket(movieSession,user);
+        ticketDao.add(ticket);
+        ShoppingCart userCart = getByUser(user);
+        userCart.getTickets().add(ticket);
+        shoppingCartDao.update(userCart);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        Optional<ShoppingCart> optionalShoppingCart = shoppingCartDao.getByUser(user);
+        if (optionalShoppingCart.isEmpty()) {
+            registerNewShoppingCart(user);
+        }
+        return optionalShoppingCart.get();
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        ShoppingCart shoppingCart = new ShoppingCart(Collections.emptyList(), user);
+        shoppingCartDao.add(shoppingCart);
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.setTickets(Collections.emptyList());
+        shoppingCartDao.update(shoppingCart);
+    }
+}
