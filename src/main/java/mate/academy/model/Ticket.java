e10ab2a01bb3de79bdf68ad6e4821b23073aa39d
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-30 16:01:20 +0200

    added changes to the class Ticket

--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -1,2 +1,46 @@
-package mate.academy.model;public class Ticket {
+package mate.academy.model;
+
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+public class Ticket {
+    private Long id;
+    @ManyToMany(fetch = FetchType.LAZY)
+    private MovieSession movieSession;
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id=" + id +
+                ", movieSession=" + movieSession +
+                ", user=" + user +
+                '}';
+    }
 }
