fc0b40cbea26f55b0016cac34b336b90aaa016c3
Vitalii Kazymyrovych <impero44@gmail.com>
2024-01-10 10:40:17 +0200

    task solved

---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,52 @@
+package mate.academy.service.impl;
+
+import java.util.List;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private TicketDao ticketDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        Ticket newTicket = new Ticket();
+        newTicket.setUser(user);
+        newTicket.setMovieSession(movieSession);
+        newTicket = ticketDao.add(newTicket);
+        ShoppingCart currentShoppingCart = shoppingCartDao
+                .getByUser(user).orElseThrow();
+        List<Ticket> newList = currentShoppingCart.getTickets();
+        newList.add(newTicket);
+        currentShoppingCart.setTickets(newList);
+        shoppingCartDao.update(currentShoppingCart);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return shoppingCartDao.getByUser(user).orElseThrow();
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        ShoppingCart newShoppingCart = new ShoppingCart();
+        newShoppingCart.setUser(user);
+        shoppingCartDao.add(newShoppingCart);
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.setTickets(List.of());
+        shoppingCartDao.update(shoppingCart);
+    }
+}
