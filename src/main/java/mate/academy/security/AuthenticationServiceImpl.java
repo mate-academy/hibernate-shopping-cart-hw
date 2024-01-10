15692d768002c533f8097cdf67437af177928cd6
Artem Peiev <slaybrute@gmail.com>
2024-01-10 18:54:34 +0200

    Add solution

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -29,8 +29,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
             User user = new User();
             user.setEmail(email);
             user.setPassword(password);
-            userService.add(user);
-            return user;
+            return userService.add(user);
         }
         throw new RegistrationException("This email is already registered.");
     }
--- src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
@@ -3,6 +3,7 @@ package mate.academy.service.impl;
 import java.time.LocalDate;
 import java.util.List;
 import mate.academy.dao.MovieSessionDao;
+import mate.academy.exception.EntityNotFoundException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.MovieSession;
@@ -19,8 +20,9 @@ public class MovieSessionServiceImpl implements MovieSessionService {
     }
 
     @Override
-    public MovieSession get(Long id) {
-        return sessionDao.get(id).get();
+    public MovieSession get(Long id) throws EntityNotFoundException {
+        return sessionDao.get(id).orElseThrow(() ->
+                new EntityNotFoundException("Cannot find movie session by id: " + id));
     }
 
     @Override
