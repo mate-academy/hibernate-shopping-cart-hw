21504fa7a44a743b5e6eda04d45973464180cb4a
Vadim <waterfallkadas@gmail.com>
2024-01-24 12:53:59 +0200

    solution 24/04

---
+++ src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
@@ -0,0 +1,35 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.TicketDao;
+import mate.academy.lib.Dao;
+import mate.academy.model.Ticket;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class TicketDaoImpl implements TicketDao {
+    @Override
+    public Ticket add(Ticket ticket) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(ticket);
+            transaction.commit();
+            return ticket;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add to DB ticket " + ticket, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
