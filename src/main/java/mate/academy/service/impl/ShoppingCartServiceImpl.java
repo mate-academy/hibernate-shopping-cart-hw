a89ad03f4775e738d57644d25d747864f5a2ca54
Mykola Horianin <anyavic2379@gmail.com>
2024-01-24 11:28:49 +0200

    added necessary changes

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.service.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import mate.academy.dao.ShoppingCartDao;
 import mate.academy.dao.TicketDao;
@@ -30,7 +31,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public ShoppingCart getByUser(User user) {
         return shoppingCartDao.getByUser(user).orElseThrow(
-                () -> new RuntimeException("Can't get shopping cart by user: " + user));
+                () -> new EntityNotFoundException("Can't get shopping cart by user: " + user));
     }
 
     @Override
