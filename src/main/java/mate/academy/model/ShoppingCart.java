aa2a52d3e597efdc08eeffaf0567b74b971dfb08
VKhmil <vovahmil@gmail.com>
2024-01-31 18:26:41 +0200

    Solution

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -2,11 +2,10 @@ package mate.academy.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -16,7 +15,6 @@ import java.util.List;
 @Table(name = "shopping_cart")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "shopping_cart_tickets",
@@ -24,6 +22,7 @@ public class ShoppingCart {
             inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
     @OneToOne
+    @MapsId
     private User user;
 
     public List<Ticket> getTickets() {
