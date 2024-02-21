e952bfbda1c330e1effbb6551ac47791c3083f51
Roman Soproniuk <151533079+RomanSoproniuk@users.noreply.github.com>
2024-02-21 15:13:03 +0200

    done hw

---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,54 @@
+package mate.academy.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private TicketDao ticketDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        Ticket newTicket = new Ticket();
+        newTicket.setUser(user);
+        newTicket.setMovieSession(movieSession);
+        Optional<ShoppingCart> shoppingCartOptional = shoppingCartDao.getByUser(user);
+        ShoppingCart shoppingCart = shoppingCartOptional.orElseThrow();
+        shoppingCart.setTickets(List.of(newTicket));
+        ticketDao.add(newTicket);
+        shoppingCartDao.update(shoppingCart);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        Optional<ShoppingCart> shoppingCartByUser = shoppingCartDao.getByUser(user);
+        return shoppingCartByUser.orElseThrow(()
+                -> new RuntimeException("Can't get shopping cart by user '" + user
+                + "' from DB"));
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUser(user);
+        shoppingCartDao.add(shoppingCart);
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.getTickets().clear();
+        shoppingCartDao.update(shoppingCart);
+    }
+}
