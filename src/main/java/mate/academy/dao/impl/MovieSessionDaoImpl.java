eae1a07319a5681277a0fdd1c04f6d013fb891a6
AdamMudrak <mudrets2000@gmail.com>
2024-03-01 23:04:57 +0200

    Predicate moviePredicate = criteriaBuilder.equal(root.get("movie"), movieId); - there is a mistake in here that needs to be addressed.

--- src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -2,6 +2,7 @@ package mate.academy.dao.impl;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import java.time.LocalDate;
@@ -11,6 +12,7 @@ import java.util.Optional;
 import mate.academy.dao.MovieSessionDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
@@ -45,22 +47,19 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
 
     @Override
     public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        int monthValue = date.getMonthValue();
+        int dayValue = date.getDayOfMonth();
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-            CriteriaQuery<MovieSession> criteriaQuery =
-                    criteriaBuilder.createQuery(MovieSession.class);
-            Root<MovieSession> root = criteriaQuery.from(MovieSession.class);
-            Predicate moviePredicate = criteriaBuilder.equal(root.get("movie"), movieId);
-            Predicate datePredicate = criteriaBuilder.between(root.get("showTime"),
-                    date.atStartOfDay(), date.atTime(END_OF_DAY));
-            Predicate allConditions = criteriaBuilder.and(moviePredicate, datePredicate);
-            criteriaQuery.select(root).where(allConditions);
-            root.fetch("movie");
-            root.fetch("cinemaHall");
-            return session.createQuery(criteriaQuery).getResultList();
+            Query<MovieSession> getAvailableSessions = session.createQuery(
+                    "FROM MovieSession movieSession WHERE "
+                            + "MONTH(movieSession.showTime) = :dBMonthValue AND "
+                            + "DAY(movieSession.showTime) = :dBDayValue",
+                    MovieSession.class);
+            getAvailableSessions.setParameter("dBMonthValue", monthValue);
+            getAvailableSessions.setParameter("dBDayValue", dayValue);
+            return getAvailableSessions.getResultList();
         } catch (Exception e) {
-            throw new DataProcessingException("Can't get available sessions for movie with id: "
-                    + movieId + " for date: " + date, e);
+            throw new DataProcessingException("Can't form a cinema halls list from DB", e);
         }
     }
 
