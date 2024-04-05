f72b677c95537f2b5f23e23717c063a32a42d348
makaroshyna <katya23225@gmail.com>
2024-04-05 13:53:31 +0300

    Remove fetch type.

--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -2,6 +2,8 @@ package mate.academy.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -10,10 +12,11 @@ import jakarta.persistence.Table;
 @Table(name = "tickets")
 public class Ticket {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     private MovieSession movieSession;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     private User user;
 
     public Long getId() {
