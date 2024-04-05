a3b07558df30b38b7f5c73b19db893a8bfdb9095
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-04-05 17:09:37 +0300

    improvemets were added

--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -14,8 +15,10 @@ public class Ticket {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
+    @JoinColumn(name = "movie_session_id")
     private MovieSession movieSession;
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Ticket() {
