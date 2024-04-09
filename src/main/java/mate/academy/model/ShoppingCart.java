cd97b9e6c94a320a640a10b26ccff4a303831a07
Viktor Kushnir <viktor.kushnir404@gmail.com>
2024-04-09 11:13:10 +0300

    resolved mentor's comments

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,7 +1,6 @@
 package mate.academy.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
