3e3da4750a6f76e0d22fe2fd38cd386817bf6aa2
Viktor Kushnir <viktor.kushnir404@gmail.com>
2024-04-09 11:11:20 +0300

    resolved mentor's comments

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,6 +1,7 @@
 package mate.academy.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
@@ -19,6 +20,7 @@ public class ShoppingCart {
     @JoinColumn(name = "user_id")
     private User user;
     @OneToMany
+    @JoinColumn(name = "shopping_cart_id")
     private List<Ticket> tickets;
 
     public ShoppingCart() {
