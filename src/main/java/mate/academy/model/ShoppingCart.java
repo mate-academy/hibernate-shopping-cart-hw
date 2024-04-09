5c80292332430628fed373d4c3596f96758534da
Nazar Klimovych <rksvjdbx@gmail.com>
2024-04-10 00:12:12 +0300

    Deleted generated value strategy in ShoppingCart class

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,8 +1,6 @@
 package mate.academy.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
@@ -15,7 +13,6 @@ import java.util.List;
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
     @JoinColumn(name = "shopping_cart_id")
