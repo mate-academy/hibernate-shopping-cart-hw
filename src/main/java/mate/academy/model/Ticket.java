d33d8519e12e1ee3d703601c868ded0dea411a69
Vlad Andriichuk <vlad150096@gmail.com>
2024-05-17 11:46:22 +0200

    Adding annotations to the Ticket model's fields.

--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -5,8 +5,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,9 +15,11 @@ public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_session_id", nullable = false)
     private MovieSession movieSession;
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public Long getId() {
