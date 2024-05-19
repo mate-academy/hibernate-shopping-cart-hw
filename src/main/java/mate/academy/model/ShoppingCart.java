7ddd524154ae3315d8239c3bfaa03cb3fdc99e5b
Dima <dmmhnk23@gmail.com>
2024-05-19 13:44:58 +0200

    finished task

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -4,9 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 
 import java.util.List;
--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -1,8 +1,9 @@
 package mate.academy.service.impl;
 
+import java.util.List;
 import mate.academy.dao.ShoppingCartDao;
 import mate.academy.dao.TicketDao;
-import mate.academy.exception.DataProcessingException;
+import java.util.NoSuchElementException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.MovieSession;
@@ -10,13 +11,6 @@ import mate.academy.model.ShoppingCart;
 import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.service.ShoppingCartService;
-import mate.academy.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.NoSuchElementException;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
@@ -55,7 +49,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Override
     public void clear(ShoppingCart shoppingCart) {
-        shoppingCart.setTickets(Collections.emptyList());
+        shoppingCart.setTickets(List.of());
         shoppingCartDao.update(shoppingCart);
     }
 }
