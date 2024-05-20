038a9b3f95b35c5ee3177d3659a18f7298bd0126
Anton <StarAntonU@gmail.com>
2024-05-20 22:21:11 +0300

    create ShoppingCart, Ticket and implement them

---
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,71 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import java.util.Optional;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can`t save shoppingCart " + shoppingCart, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<ShoppingCart> getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<ShoppingCart> getShoppingCartByUser = session.createQuery("from ShoppingCart sh "
+                    + "left join fetch sh.tickets "
+                    + "left join fetch sh.user "
+                    + "where sh.user = :user", ShoppingCart.class);
+            getShoppingCartByUser.setParameter("user", user);
+            return getShoppingCartByUser.uniqueResultOptional();
+        }
+
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.merge(shoppingCart);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can`t update shoppingCart " + shoppingCart, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,73 @@
+package mate.academy.service.impl;
+
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.dao.UserDao;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
+import mate.academy.service.ShoppingCartService;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private TicketDao ticketDao;
+    @Inject
+    private UserDao userDao;
+    @Inject
+    private AuthenticationService authenticationService;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        Ticket ticket = new Ticket();
+        ticket.setMovieSession(movieSession);
+        ticket.setUser(user);
+        ticketDao.add(ticket);
+        User userFromDB = userDao.findByEmail(user.getEmail()).get();
+        ShoppingCart shoppingCart = shoppingCartDao.getByUser(userFromDB).get();
+        List<Ticket> tickets = shoppingCart.getTickets();
+        tickets.add(ticket);
+        shoppingCartDao.update(shoppingCart);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return shoppingCartDao.getByUser(user).orElseThrow(()
+                -> new DataProcessingException("Can`n get user " + user + " from DB"));
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        User userFromDB;
+        try {
+            userFromDB = authenticationService.register(user.getEmail(), user.getPassword());
+        } catch (RegistrationException e) {
+            try {
+                userFromDB = authenticationService.login(user.getEmail(), user.getPassword());
+            } catch (AuthenticationException ex) {
+                throw new DataProcessingException("Can`t register user " + user, ex);
+            }
+        }
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUser(userFromDB);
+        shoppingCartDao.add(shoppingCart);
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.setTickets(new ArrayList<>());
+        shoppingCartDao.update(shoppingCart);
+    }
+}
