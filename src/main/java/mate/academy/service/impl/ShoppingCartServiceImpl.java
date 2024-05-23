daaf689788c03f88bc795f2f6e8b99a6467a954c
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-05-24 00:13:20 +0300

    fix checkstyle errors

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -1,8 +1,7 @@
 package mate.academy.service.impl;
 
-import java.util.List;
-
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
 import mate.academy.dao.ShoppingCartDao;
 import mate.academy.dao.TicketDao;
 import mate.academy.lib.Inject;
