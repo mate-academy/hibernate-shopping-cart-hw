15e9837626d6c9bf4a450bee6fbd814f2e600007
Yana Bilyk <bilyk.yn@gmail.com>
2024-07-09 17:31:19 -0400

    Created models of ShoppingCart and Tickets, added hibernate.cfg.xml file with all entity;

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,58 @@
+package mate.academy.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tickets")
+class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_session_id")
+    private MovieSession movieSession;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Ticket() {
+    }
+
+    public Ticket(MovieSession movieSession, User user) {
+        this.movieSession = movieSession;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Tickets { "
+                + "id = " + id
+                + ", movie session = " + movieSession
+                + ", user = " + user + '\''
+                + " }";
+    }
+}
