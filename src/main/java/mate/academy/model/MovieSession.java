83f49c995b635828a8ec95be96c2c3f07fe196e5
Illya Pantazi <illios.piv@gmail.com>
2024-08-12 05:52:52 +0200

    The solution of the `hibernate-shopping-cart-hw`: Created interfaces: `ShoppingCartDao`, `TicketDao`, `ShoppingCartService`. Created a file `hibernate.cfg.xml` and edited `pom.xml`. Created classes: `ShoppingCartDaoImpl`, `TicketDaoImpl`, `ShoppingCart`, `Ticket`, `ShoppingCartServiceImpl`. Edited classes: `Main`, `MovieSession`, `AuthenticationServiceImpl`.

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -1,10 +1,12 @@
 package mate.academy.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
@@ -15,10 +17,16 @@ public class MovieSession {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
     private Movie movie;
+
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cinema_hall_id")
     private CinemaHall cinemaHall;
+
+    @Column(name = "show_time")
     private LocalDateTime showTime;
 
     public Long getId() {
---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,54 @@
+package mate.academy.service.impl;
+
+import jakarta.persistence.EntityNotFoundException;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private TicketDao ticketDao;
+
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        final ShoppingCart shoppingCartFromDB = shoppingCartDao.getByUser(user).orElseThrow(()
+                -> new EntityNotFoundException("The shopping cart isn't exist in DB for this "
+                + "user: " + user));
+        Ticket ticket = new Ticket();
+        ticket.setUser(user);
+        ticket.setMovieSession(movieSession);
+        ticketDao.add(ticket);
+        shoppingCartFromDB.getTickets().add(ticket);
+        shoppingCartDao.update(shoppingCartFromDB);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return shoppingCartDao.getByUser(user).orElseThrow(()
+                -> new EntityNotFoundException("The shopping cart isn't exist in DB for this "
+                + "user: " + user));
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUser(user);
+        shoppingCartDao.add(shoppingCart);
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.getTickets().clear();
+        shoppingCartDao.update(shoppingCart);
+    }
+}
