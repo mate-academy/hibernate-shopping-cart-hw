9c2eea09ca23ed6d3d352bafd5a2167db0a0c61d
ChabVlad <vlad.chabak@gmail.com>
2024-08-27 12:45:57 +0400

    looks good

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -6,7 +6,9 @@ import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
 import mate.academy.service.UserService;
+import mate.academy.service.impl.ShoppingCartServiceImpl;
 import mate.academy.util.HashUtil;
 
 @Service
@@ -25,11 +27,13 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User register(String email, String password) throws RegistrationException {
+        ShoppingCartService shoppingCartService = new ShoppingCartServiceImpl();
         if (userService.findByEmail(email).isEmpty()) {
             User user = new User();
             user.setEmail(email);
             user.setPassword(password);
             userService.add(user);
+            shoppingCartService.registerNewShoppingCart(user);
             return user;
         }
         throw new RegistrationException("This email is already registered.");
