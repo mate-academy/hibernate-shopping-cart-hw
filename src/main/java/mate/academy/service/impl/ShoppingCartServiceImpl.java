e421835b023a336d4a97d68e47bf12b8353e5848
Svyatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2024-08-29 09:56:59 -0400

    hibernate-shopping-cart-hw task solution

--- src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -1,7 +1,6 @@
 package mate.academy.service.impl;
 
 import jakarta.persistence.EntityNotFoundException;
-import java.util.List;
 import mate.academy.dao.ShoppingCartDao;
 import mate.academy.dao.TicketDao;
 import mate.academy.lib.Inject;
