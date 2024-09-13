b4121a99f0dcc883f777cd324c2f8ebc16bc6ec3
Andrey Chertykovtsev <chertiav@gmail.com>
2024-09-13 16:11:54 +0200

    Add ShoppingCart and Ticket models - implemented ShoppingCart and Ticket entities.

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,72 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MovieSession movieSession;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ShoppingCart shoppingCart;
+
+    public Ticket() {
+    }
+
+    public Ticket(MovieSession movieSession, User user, ShoppingCart shoppingCart) {
+        this.movieSession = movieSession;
+        this.user = user;
+        this.shoppingCart = shoppingCart;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{"
+               + "id=" + id
+               + ", movieSession=" + movieSession.getId()
+               + '}';
+    }
+}
---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,42 @@
+package mate.academy.service.impl;
+
+import jakarta.persistence.EntityNotFoundException;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private TicketDao ticketDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        ticketDao.add(new Ticket(movieSession, user, getByUser(user)));
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return shoppingCartDao.getByUser(user).orElseThrow(() ->
+                new EntityNotFoundException("Can't get shoppingCart by user" + user));
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        shoppingCartDao.add(new ShoppingCart(user));
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        shoppingCart.getTickets().clear();
+        shoppingCartDao.update(shoppingCart);
+    }
+}
