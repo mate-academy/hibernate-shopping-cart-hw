b457079d2168d642077fbc7a3299bfdf08918f18
OD <sdenisenko055@gmail.com>
2024-09-15 15:13:23 +0300

    result

--- src/main/java/mate/academy/model/Ticket.java
+++ src/main/java/mate/academy/model/Ticket.java
@@ -5,18 +5,21 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tickets")
 public class Ticket {
-   @Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-   @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_session_id")
     private MovieSession movieSession;
-   @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Long getId() {
--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -1,11 +1,13 @@
 package mate.academy.security;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.Optional;
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
 import mate.academy.service.UserService;
 import mate.academy.util.HashUtil;
 
@@ -13,6 +15,8 @@ import mate.academy.util.HashUtil;
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
     private UserService userService;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
@@ -30,6 +34,11 @@ public class AuthenticationServiceImpl implements AuthenticationService {
             user.setEmail(email);
             user.setPassword(password);
             userService.add(user);
+            shoppingCartService
+                    .registerNewShoppingCart(userService
+                            .findByEmail(user.getEmail())
+                            .orElseThrow(() -> new EntityNotFoundException(
+                                    "Can't find user for registration shopping cart")));
             return user;
         }
         throw new RegistrationException("This email is already registered.");
