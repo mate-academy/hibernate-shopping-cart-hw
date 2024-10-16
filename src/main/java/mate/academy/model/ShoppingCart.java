7094d60bc239d17220abd8406b36f771771e8560
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-10-16 11:25:54 +0300

    task done

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,7 +1,16 @@
 package mate.academy.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.List;
 
 @Entity
@@ -19,9 +28,6 @@ public class ShoppingCart {
     @MapsId
     @JoinColumn(name = "user_id")
     private User user;
-
-    public ShoppingCart() {
-    }
 
     public List<Ticket> getTickets() {
         return tickets;
--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import java.util.Arrays;
 
 @Entity
 @Table(name = "users")
