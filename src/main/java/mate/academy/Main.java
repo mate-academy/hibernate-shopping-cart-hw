caf41a666c6bcf24b26543103cf9344cc9a2eda9
Igor Neminuschii <igorgorslav@gmail.com>
2024-11-28 03:03:24 +0200

    added Ticket and ShoppingCart entities to te Cinema project

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,16 +2,28 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.ShoppingCartService;
+import mate.academy.service.UserService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+    private static final String EMAIL = "bob@email.com";
+    private static final String PASSWORD = "123456";
+
+    public static void main(String[] args) throws RegistrationException {
+        MovieService movieService =
+                (MovieService) INJECTOR.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
@@ -27,7 +39,8 @@ public class Main {
         secondCinemaHall.setCapacity(200);
         secondCinemaHall.setDescription("second hall with capacity 200");
 
-        CinemaHallService cinemaHallService = null;
+        CinemaHallService cinemaHallService =
+                (CinemaHallService) INJECTOR.getInstance(CinemaHallService.class);
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
 
@@ -44,12 +57,32 @@ public class Main {
         yesterdayMovieSession.setMovie(fastAndFurious);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-        MovieSessionService movieSessionService = null;
+        MovieSessionService movieSessionService =
+                (MovieSessionService) INJECTOR.getInstance(MovieSessionService.class);
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                 fastAndFurious.getId(), LocalDate.now()));
+
+        UserService userService =
+                (UserService) INJECTOR.getInstance(UserService.class);
+
+        AuthenticationService authenticationService =
+                (AuthenticationService) INJECTOR.getInstance(
+                        AuthenticationService.class);
+        authenticationService.register(EMAIL, PASSWORD);
+        User bob = userService.findByEmail(EMAIL).get();
+        System.out.println(bob);
+
+        ShoppingCartService cartService =
+                (ShoppingCartService) INJECTOR.getInstance(ShoppingCartService.class);
+
+        cartService.addSession(tomorrowMovieSession, bob);
+        cartService.addSession(yesterdayMovieSession, bob);
+
+        ShoppingCart cart = cartService.getByUser(bob);
+        cartService.clear(cart);
     }
 }
