966d31f5160ce3c79aed09f8cbf8f9a9281691fb
Ostap <ostzat@gmail.com>
2025-01-04 19:44:25 +0100

    solution

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -6,6 +6,7 @@ import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
 import mate.academy.service.UserService;
 import mate.academy.util.HashUtil;
 
@@ -14,6 +15,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
     private UserService userService;
 
+    @Inject
+    private ShoppingCartService shoppingCartService;
+    
     @Override
     public User login(String email, String password) throws AuthenticationException {
         Optional<User> userFromDb = userService.findByEmail(email);
