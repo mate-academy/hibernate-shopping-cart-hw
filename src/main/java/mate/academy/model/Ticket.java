b4ba65bee28ef778bf18985a2425ca31ba4c6b2b
Igor Gunder <gunderigorv@gmail.com>
2025-01-04 21:39:29 +0200

    I made a shopping cart.

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,61 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_session_id", nullable = false)
+    private MovieSession movieSession;
+
+    public Ticket() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public MovieSession getMovieSession() {
+        return movieSession;
+    }
+
+    public void setMovieSession(MovieSession movieSession) {
+        this.movieSession = movieSession;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{"
+                + "id=" + id
+                + ", user=" + (user != null ? "User{id=" + user.getId() + "}" : "null")
+                + ", movieSession=" + (movieSession != null
+                ? "MovieSession{id=" + movieSession.getId() + "}" : "null")
+                + '}';
+    }
+}
