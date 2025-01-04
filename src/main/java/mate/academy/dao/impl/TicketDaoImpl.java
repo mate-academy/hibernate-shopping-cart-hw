1db63565b383fb32f9033174295672978df0dd50
Igor Gunder <gunderigorv@gmail.com>
2025-01-04 22:02:16 +0200

    I made a shopping cart.

--- src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/TicketDaoImpl.java
@@ -11,7 +11,7 @@ import org.hibernate.Transaction;
 
 @Dao
 public class TicketDaoImpl implements TicketDao {
-    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    private static final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
     @Override
     public Ticket add(Ticket ticket) {
