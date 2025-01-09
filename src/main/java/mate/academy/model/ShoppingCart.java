5b45c3ec6a3c076441ed7d84ac22f94f90cef1c2
rus1980707 <rus1980707@gmail.com>
2025-01-09 13:14:04 +0200

    hibernate-shopping-cart

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -1,7 +1,16 @@
 package mate.academy.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.List;
 
 @Entity
@@ -11,7 +20,6 @@ public class ShoppingCart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToOne
-   // @MapsId
     @JoinColumn(name = "id")
     private User user;
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
