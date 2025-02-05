57f5957e020f165372528b53d039ac139b43a9a5
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2025-02-05 17:44:18 +0200

    valid solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,21 +2,34 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.ShoppingCartService;
+import mate.academy.service.UserService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) throws RegistrationException, AuthenticationException {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
+        Movie transformers = new Movie("Transformers");
+        transformers.setDescription("Fantastic film about war between robots from another planet.");
         movieService.add(fastAndFurious);
+        movieService.add(transformers);
         System.out.println(movieService.get(fastAndFurious.getId()));
+        System.out.println(movieService.get(transformers.getId()));
         movieService.getAll().forEach(System.out::println);
 
         CinemaHall firstCinemaHall = new CinemaHall();
@@ -27,10 +40,10 @@ public class Main {
         secondCinemaHall.setCapacity(200);
         secondCinemaHall.setDescription("second hall with capacity 200");
 
-        CinemaHallService cinemaHallService = null;
+        CinemaHallService cinemaHallService =
+                (CinemaHallService) injector.getInstance(CinemaHallService.class);
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
-
         System.out.println(cinemaHallService.getAll());
         System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
 
@@ -41,15 +54,34 @@ public class Main {
 
         MovieSession yesterdayMovieSession = new MovieSession();
         yesterdayMovieSession.setCinemaHall(firstCinemaHall);
-        yesterdayMovieSession.setMovie(fastAndFurious);
+        yesterdayMovieSession.setMovie(transformers);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-        MovieSessionService movieSessionService = null;
+        MovieSessionService movieSessionService =
+                (MovieSessionService) injector.getInstance(MovieSessionService.class);
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
-
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
                 fastAndFurious.getId(), LocalDate.now()));
+
+        AuthenticationService authenticationService =
+                (AuthenticationService) injector.getInstance(AuthenticationService.class);
+        User ihor = authenticationService.register("sidorenko.igorek@gmail.com", "mysql666");
+        System.out.println(authenticationService.login("sidorenko.igorek@gmail.com", "mysql666"));
+
+        UserService userService = (UserService) injector.getInstance(UserService.class);
+        System.out.println(userService.findByEmail("sidorenko.igorek@gmail.com"));
+
+        ShoppingCartService shoppingCartService =
+                (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
+
+        User nelia = authenticationService.register("sidorenko.nelia@gmail.com", "mysql777");
+        shoppingCartService.addSession(tomorrowMovieSession, ihor);
+        shoppingCartService.addSession(yesterdayMovieSession, ihor);
+        shoppingCartService.addSession(tomorrowMovieSession, nelia);
+
+        System.out.println(shoppingCartService.getByUser(ihor));
+        shoppingCartService.clear(shoppingCartService.getByUser(ihor));
     }
 }
