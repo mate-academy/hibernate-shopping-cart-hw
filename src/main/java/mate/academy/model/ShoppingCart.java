f5edb5855d8acccab7460d41025249c193f345a7
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-04-05 19:42:50 +0200

    review

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -30,7 +29,6 @@ public class ShoppingCart {
     @OneToMany
     private List<Ticket> tickets;
 
-    @MapsId
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
